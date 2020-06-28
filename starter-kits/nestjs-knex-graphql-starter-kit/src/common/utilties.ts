0f55d70cf9f2a6f7c34af8f0adf3a24df4febeba
Faizan AH <faizan@aiondigital.com>
2020-06-28 13:34:15 +0500

    feat: [APS-452] added utilities file

---
+++ starter-kits/nestjs-knex-graphql-starter-kit/src/common/utilties.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+
+export function fullPath(fileOrDir) {
+  return path.join(__dirname, fileOrDir);
+}
+
+export function isTruthy(bool: string): boolean {
+  try {
+    return bool.toLowerCase() === 'true';
+  } catch (e) {
+    return false;
+  }
+}
+/**
+ * normalizePort port string | number
+ * @param param
+ */
+export function normalizePort(param: number | string): number | string {
+  const portNumber: number =
+    typeof param === 'string' ? parseInt(param, 10) : param;
+  if (isNaN(portNumber)) return param;
+  else if (portNumber >= 0) return portNumber;
+}
+/**
+ * generate random string
+ * @param length
+ */
+export function generateRandomString(length: number): string {
+  return Math.random()
+    .toString(36)
+    .replace(/[^a-zA-Z0-9]+/g, '')
+    .substr(0, length);
+}
