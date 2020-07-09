e330ce6a70efb354a63d84b4443d7d5b96099bf6
Rashid Aslam <rashid@aiondigital.com>
2020-07-09 23:35:10 +0500

    moved mostly used imports to separate module

--- starter-kits/angular-starter-kit/src/app/features/user/user.module.ts
+++ starter-kits/angular-starter-kit/src/app/features/user/user.module.ts
@@ -3,45 +3,19 @@ import { CommonModule } from "@angular/common";
 
 import { UserRoutingModule } from "./user-routing.module";
 import { UserComponent } from "./views/user/user.component";
-import { MatButtonModule } from "@angular/material/button";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatRippleModule } from "@angular/material/core";
-import { MatSelectModule } from "@angular/material/select";
 import { FuseSharedModule } from "@fuse/shared.module";
 import { UserFormComponent } from "./components/user-form/user-form.component";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { SharedModule } from '@shared/shared.module';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MaterialFormsModule } from '@shared/modules/material-forms/material-forms.module';
 @NgModule({
     declarations: [UserComponent, UserFormComponent],
     entryComponents: [UserFormComponent],
     imports: [
         CommonModule,
         SharedModule,
-        MatButtonModule,
-        MatMenuModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatRippleModule,
-        MatDatepickerModule,
-        MatSelectModule,
-        MatTooltipModule,
         FuseSharedModule,
+        MaterialFormsModule,
         UserRoutingModule,
-        MatButtonModule,
-        MatFormFieldModule,
-        MatToolbarModule,
-        MatDialogModule,
-        MatTableModule,
-        MatIconModule,
-        MatRippleModule,
     ],
 })
 export class UserModule {}
