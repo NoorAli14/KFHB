7017483fa6b49f1260508937625112610616f3d3
Faizan AH <faizan@aiondigital.com>
2020-07-12 01:45:25 +0500

    fix: [APS-452] added swagger documentation

--- starter-kits/nestjs-knex-graphql-starter-kit/src/core/middlewares/kernel.middleware.ts
+++ starter-kits/nestjs-knex-graphql-starter-kit/src/core/middlewares/kernel.middleware.ts
@@ -4,12 +4,12 @@ import { Swagger } from '@core/providers/swagger.provider';
 
 export class KernelMiddleware {
   public static init(
-    _app: INestApplication,
-    _config: ConfigurationService,
+    app: INestApplication,
+    config: ConfigurationService,
   ): INestApplication {
-    if (_config.IS_SWAGGER_ENABLED) {
-      _app = Swagger.init(_app, _config);
+    if (config.IS_SWAGGER_ENABLED) {
+      app = Swagger.init(app, config);
     }
-    return _app;
+    return app;
   }
 }
