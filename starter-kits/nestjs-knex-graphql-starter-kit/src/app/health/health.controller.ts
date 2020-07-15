15cb3b6e02c38272c8f4d9b3b6c009baf45d98c2
Faizan AH <faizan@aiondigital.com>
2020-07-16 01:02:33 +0500

    docs: [APS-452] typo fixes

--- starter-kits/nestjs-knex-graphql-starter-kit/src/app/health/health.controller.ts
+++ starter-kits/nestjs-knex-graphql-starter-kit/src/app/health/health.controller.ts
@@ -2,18 +2,11 @@ import {
   DiskHealthIndicator,
   MemoryHealthIndicator,
   DNSHealthIndicator,
-HealthCheckService,
-HealthCheck
+  HealthCheckService,
+  HealthCheck,
 } from '@nestjs/terminus';
-import {
-  Controller,
-  Get
-  } from '@nestjs/common';
-import {
-  ApiResponse,
-  ApiTags,
-  ApiOkResponse,
-} from '@nestjs/swagger';
+import { Controller, Get } from '@nestjs/common';
+import { ApiResponse, ApiTags, ApiOkResponse } from '@nestjs/swagger';
 
 @ApiTags('Health')
 @Controller('health')
@@ -21,19 +14,23 @@ export class HealthController {
   constructor(
     private readonly health: HealthCheckService,
     private readonly dns: DNSHealthIndicator,
-    private readonly memory: MemoryHealthIndicator
-  ) { }
+    private readonly memory: MemoryHealthIndicator,
+    private readonly disk: DiskHealthIndicator,
+  ) {}
 
   @Get()
   @HealthCheck()
-  @ApiResponse({ status: 200,     
-  description: 'Health status of the service',
- })
+  @ApiResponse({ status: 200, description: 'Health status of the service' })
   healthCheck() {
     return this.health.check([
       async () => this.dns.pingCheck('google', 'https://google.com'),
       async () => this.memory.checkRSS('memory_rss', 3000 * 1024 * 1024),
       async () => this.memory.checkHeap('memory_heap', 200 * 1024 * 1024),
+      // async () =>
+      //   this.disk.checkStorage('storage', {
+      //     threshold: 2 * 1024 * 1024 * 1024,
+      //     path: '/',
+      //   }),
     ]);
   }
 }
