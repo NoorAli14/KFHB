6257ffdc7e9998fd332f024675a11701c083909e
Rashid Aslam <rashid@aiondigital.com>
2020-07-15 15:59:07 +0500

    fixing after merge issue

--- starter-kits/angular-starter-kit/src/app/auth/auth.module.ts
+++ starter-kits/angular-starter-kit/src/app/auth/auth.module.ts
@@ -7,14 +7,25 @@ import { FuseSharedModule } from "@fuse/shared.module";
 import { ForgotPasswordComponent } from './views/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './views/reset-password/reset-password.component';
 import { MaterialFormsModule } from '@shared/modules/material-forms/material-forms.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
     declarations: [LoginComponent,  ForgotPasswordComponent, ResetPasswordComponent],
     imports: [
         CommonModule,
-        AuthRoutingModule,
         MaterialFormsModule,
         FuseSharedModule,
+        AuthRoutingModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+
     ],
 })
 export class AuthModule {}
