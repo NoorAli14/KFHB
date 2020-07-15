cc74b3953b6d2eb8571d4151446c629c2a4243fd
Rashid Aslam <rashid@aiondigital.com>
2020-07-15 19:08:41 +0500

    phone verification compononent added

--- retail/webadmin/src/app/auth/views/phone-verification/phone-verification.component.ts
+++ retail/webadmin/src/app/auth/views/phone-verification/phone-verification.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FuseConfigService } from "@fuse/services/config.service";
 
 @Component({
-  selector: 'app-phone-verification',
-  templateUrl: './phone-verification.component.html',
-  styleUrls: ['./phone-verification.component.scss']
+    selector: "app-phone-verification",
+    templateUrl: "./phone-verification.component.html",
+    styleUrls: ["./phone-verification.component.scss"],
 })
 export class PhoneVerificationComponent implements OnInit {
+    recoverPasswordForm: FormGroup;
 
-  constructor() { }
+    constructor(
+        private _fuseConfigService: FuseConfigService,
+        private _formBuilder: FormBuilder
+    ) {
+        // Configure the layout
+        this._fuseConfigService.config = {
+            layout: {
+                navbar: {
+                    hidden: true,
+                },
+                toolbar: {
+                    hidden: true,
+                },
+                footer: {
+                    hidden: true,
+                },
+                sidepanel: {
+                    hidden: true,
+                },
+            },
+        };
+    }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.recoverPasswordForm = this._formBuilder.group({
+            code: [
+                "",
+                [Validators.required, Validators.min(6), Validators.max(6)],
+            ],
+        });
+    }
 }
