ae6c77ec44ccf0a6462092afffc1280f7bc5935b
Faizan AH <faizan@aiondigital.com>
2020-07-21 02:03:12 +0500

    feat: [APS-543] added correlation middleware

---
+++ retail/middleware/onboarding.gateway/src/core/middlewares/correlation.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+import { uuid } from '@common/utilities';
+import { X_CORRELATION_KEY } from '@common/constants';
+export const CorrelationMiddleware = (): Function => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    const correlationId = req.header(X_CORRELATION_KEY) || uuid();
+    // make sure this is lower-cased, otherwise downstream stuff will barf.
+    req.headers[X_CORRELATION_KEY] = correlationId;
+    res.set(X_CORRELATION_KEY, correlationId);
+    next();
+  };
+};
