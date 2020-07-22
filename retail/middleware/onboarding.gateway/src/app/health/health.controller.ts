ea4cf78822a75eba3f6da65d8a703b4b401c540c
Faizan AH <faizan@aiondigital.com>
2020-07-22 19:21:07 +0500

    docs: [APS-543] added comments in health check section

--- retail/middleware/onboarding.gateway/src/app/health/health.controller.ts
+++ retail/middleware/onboarding.gateway/src/app/health/health.controller.ts
@@ -22,14 +22,19 @@ export class HealthController {
   @HealthCheck()
   @ApiResponse({ status: 200, description: 'Health status of the service' })
   healthCheck(): Promise<any> {
+    //Health indicator executes a check of a service, whether it is in a healthy or unhealthy state
     return this.health.check([
       async (): Promise<any> =>
+        // DNS health Checker. 
         this.dns.pingCheck('google', 'https://google.com'),
       async (): Promise<any> =>
+        // RSS Memory health Checker.
         this.memory.checkRSS('memory_rss', 3000 * 1024 * 1024),
       async (): Promise<any> =>
+        // Heap Memory health Checker.
         this.memory.checkHeap('memory_heap', 200 * 1024 * 1024),
       // async (): Promise<any> =>
+      // // Disk Space stoarge Checker.
       //   this.disk.checkStorage('storage', {
       //     threshold: 2 * 1024 * 1024 * 1024,
       //     path: '/',
