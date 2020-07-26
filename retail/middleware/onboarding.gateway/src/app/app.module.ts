93d655ad2d05fa6663d25ab0921ed761d7d34f8f
Faizan AH <faizan@aiondigital.com>
2020-07-26 14:28:33 +0500

    docs: [APS-543] rename v1Moodule

--- retail/middleware/onboarding.gateway/src/app/app.module.ts
+++ retail/middleware/onboarding.gateway/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
-import { V1Module } from './v1/v1.module';
+import { ModuleV1 } from './v1/v1.module';
 import { CommonModule } from '@common/common.module';
 @Module({
-  imports: [HealthModule, CommonModule, V1Module],
+  imports: [HealthModule, CommonModule, ModuleV1],
 })
 export class AppModule {}
