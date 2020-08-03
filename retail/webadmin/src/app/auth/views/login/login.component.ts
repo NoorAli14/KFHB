7b62b03eabbcdd351b71b12a65be309f4c4f8d85
Rashid Aslam <rashidaslam75@gmail.com>
2020-08-03 12:24:46 -0700

    configured dynamic sidebar

--- retail/webadmin/src/app/auth/views/login/login.component.ts
+++ retail/webadmin/src/app/auth/views/login/login.component.ts
@@ -1,50 +1,63 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from "@angular/router";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+    FormBuilder,
+    FormGroup,
+    Validators,
+    FormControl,
+} from "@angular/forms";
 
-import { FuseConfigService } from '@fuse/services/config.service';
-import { fuseAnimations } from '@fuse/animations';
+import { FuseConfigService } from "@fuse/services/config.service";
+import { fuseAnimations } from "@fuse/animations";
+import { AuthenticationService } from "@core/services/auth/authentication.service";
 
 @Component({
-    selector     : 'app-login',
-    templateUrl  : './login.component.html',
-    styleUrls    : ['./login.component.scss'],
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.scss"],
     encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+    animations: fuseAnimations,
 })
-export class LoginComponent implements OnInit
-{
+export class LoginComponent implements OnInit {
     loginForm: FormGroup;
     constructor(
         private _fuseConfigService: FuseConfigService,
-    )
-    {
+        private _authService: AuthenticationService,
+        private router: Router
+    ) {
         // Configure the layout
         this._fuseConfigService.config = {
             layout: {
-                navbar   : {
-                    hidden: true
+                navbar: {
+                    hidden: true,
                 },
-                toolbar  : {
-                    hidden: true
+                toolbar: {
+                    hidden: true,
                 },
-                footer   : {
-                    hidden: true
+                footer: {
+                    hidden: true,
                 },
                 sidepanel: {
-                    hidden: true
-                }
-            }
+                    hidden: true,
+                },
+            },
         };
     }
 
-    ngOnInit(): void
-    {
+    ngOnInit(): void {
         this.loginForm = new FormGroup({
-            email: new FormControl('', [
-                Validators.required, Validators.email
-            ]),
+            email: new FormControl("", [Validators.required, Validators.email]),
             password: new FormControl("", [Validators.required]),
-            
         });
+    }
+    onSubmit() {
+        this._authService.login(this.loginForm.value).subscribe(
+            (response) => {
+                this.router.navigateByUrl("/ent/user");
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
     }
 }
