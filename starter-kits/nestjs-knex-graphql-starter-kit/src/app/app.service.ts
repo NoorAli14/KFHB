99cd8c9c3bb725a6d78e0c47c901edfb5bdb529a
Faizan AH <faizan@aiondigital.com>
2020-08-09 20:54:53 +0500

    feat: [APS-452] added api info endpoint

---
+++ starter-kits/nestjs-knex-graphql-starter-kit/src/app/app.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigurationService } from '@rubix/common/configuration/configuration.service';
+@Injectable()
+export class AppService {
+
+  constructor() {
+  }
+
+  getInfo(configService: ConfigurationService): Record<string,any> {
+    const links = {
+      links: {}
+    };
+    links.links['health'] =`${configService.APPLICATION_HOST}/${configService.APP.API_URL_PREFIX}/health`;
+    if(configService.SWAGGER.ENABLE) {
+      links.links['swagger'] =`${configService.APPLICATION_HOST}${configService.SWAGGER.ROUTE}`;
+    }
+    if(configService.GRAPHQL.PLAYGROUND) {
+      links.links['graphql'] =`${configService.APPLICATION_HOST}${configService.GRAPHQL.ROUTE}`;
+    }
+    return {
+      name: configService.APP.NAME,
+      version: configService.APP.VERSION,
+      environment: configService.APP.ENVIRONMENT,
+      description: configService.APP.DESCRIPTION,
+      ...links
+    }
+  }
+}
