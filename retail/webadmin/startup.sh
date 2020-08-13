9631dbed0b0e40c338ea49088729dd17263f36e1
Rashid Aslam <rashidaslam75@gmail.com>
2020-08-13 22:31:26 +0500

    fixed deployment issue

--- retail/webadmin/startup.sh
+++ retail/webadmin/startup.sh
@@ -1,4 +1,4 @@
 echo "Script started ..."
 
-# envsubst < "/usr/share/nginx/html/assets/env.template.js" > "/usr/share/nginx/html/assets/env.js"
+envsubst < "/usr/share/nginx/html/assets/env.template.js" > "/usr/share/nginx/html/assets/env.js"
 nginx -g 'daemon off;'
