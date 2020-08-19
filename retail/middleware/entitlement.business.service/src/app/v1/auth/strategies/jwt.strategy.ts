6ba46f62c2ad8c702d74052edf41b6af0810a75f
Faizan AH <faizan@aiondigital.com>
2020-08-19 16:01:07 +0500

    feat: added auth strategies

---
+++ retail/middleware/entitlement.business.service/src/app/v1/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { ConfigurationService } from '@common/configuration/configuration.service';
+import { UserService } from '@app/v1/users/users.service';
+import { X_ACCESS_TOKEN } from '@common/constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigurationService,
+    private readonly userService: UserService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.headers[X_ACCESS_TOKEN] as string;
+        },
+      ]),
+      ignoreExpiration: configService.JWT.IGNORE_EXPIRY,
+      secretOrKey: configService.JWT.SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    return this.userService.findOne(payload.id);
+  }
+}
