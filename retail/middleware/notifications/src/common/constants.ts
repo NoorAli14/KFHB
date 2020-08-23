5ecf26eb877f199a60b6ce374b71546e78d2e350
aahmadraza <mraza@aiondigital.com>
2020-08-23 06:33:19 +0500

    resolve comments

--- retail/middleware/notifications/src/common/constants.ts
+++ retail/middleware/notifications/src/common/constants.ts
@@ -3,7 +3,7 @@ export const TABLE = {
 };
 export const DATABASE_UUID_METHOD = 'NEWID()';
 
-export const DATABASE_MIGRATION_TABLE_NAME = 'rubix_migrations';
+export const DATABASE_MIGRATION_TABLE_NAME = 'RUBIX_NOTIFICATION_MIGRATIONS';
 export const DATABASE_MIGRATION_DIRECTORY = 'src/core/database/migrations';
 export const DATABASE_SEED_DIRECTORY = 'src/core/database/seeds';
 
