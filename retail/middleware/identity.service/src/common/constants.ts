4f96515bf7b18c1f6eaddedb28a574618161aa58
Faizan AH <faizan@aiondigital.com>
2020-08-24 01:55:30 +0500

    docs: [APS-478] update constants

--- retail/middleware/identity.service/src/common/constants.ts
+++ retail/middleware/identity.service/src/common/constants.ts
@@ -1,6 +1,6 @@
 export const TABLE = {
   SESSION: 'IDT_SESSION',
-  USER: 'T_USER',
+  DOCUMENT_TYPE: 'IDT_DOCUMENT_TYPE',
   POST: 'T_POST',
   COMMENT: 'T_COMMENT',
 };
@@ -9,3 +9,11 @@ export const DATABASE_UUID_METHOD = 'NEWID()';
 export const DATABASE_MIGRATION_TABLE_NAME = 'RUBIX_IDENTITY_MIGRATIONS';
 export const DATABASE_MIGRATION_DIRECTORY = 'src/core/database/migrations';
 export const DATABASE_SEED_DIRECTORY = 'src/core/database/seeds';
+export const DOCUMENT_TYPES = [
+  'SELFIE',
+  'LIVENESS',
+  'PASSPORT',
+  'DRIVING_LICENSE',
+  'FRONT_SIDE',
+  'BACK_SIDE',
+];
