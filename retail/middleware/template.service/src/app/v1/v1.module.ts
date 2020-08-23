98c5d837b9e08bfe85ccfb7a903cb2f4ee2c1546
Fahid Nasir <fahidnasir@hotmail.com>
2020-08-23 18:25:34 +0500

    Deleted: TemplateQuestions Module and Table (Nested Approach is done).

--- retail/middleware/template.service/src/app/v1/v1.module.ts
+++ retail/middleware/template.service/src/app/v1/v1.module.ts
@@ -10,13 +10,16 @@ import { SectionsModule } from './sections/sections.module';
 import { TemplatesModule } from './templates/templates.module';
 import { DataloaderModule } from '@core/dataloaders/loader.module';
 import { RepositoryModule } from '@core/repository/repository.module';
-import { TemplateQuestionsModule } from './template-questions/template-questions.module';
+// import { TemplateQuestionsModule } from './template-questions/template-questions.module';
 
 @Module({
   imports: [
     RepositoryModule,
     DataloaderModule,
-    TemplateQuestionsModule,
+    // ---- DEPRECATED ----
+    // Not using this Module because straight one-many relations are added for Template-Section, Section-Question.
+    // TemplateQuestionsModule,
+    // ---- END DEPRECATED ----
     TemplateResponsesModule,
     TemplatesModule,
     OptionsModule,
