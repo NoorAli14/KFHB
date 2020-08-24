5d2812042bf0ffe1921a4b78776feebca0ebc3f3
Faizan AH <faizan@aiondigital.com>
2020-08-25 01:32:14 +0500

    feat: [APS-475] added validation on permission module

--- retail/middleware/entitlement.business.service/src/app/v1/modules/modules.service.ts
+++ retail/middleware/entitlement.business.service/src/app/v1/modules/modules.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { Module } from './module.entity';
 import { ModuleDto } from './module.dto';
 import { toGraphql } from '@common/utilities';
@@ -6,73 +10,56 @@ import { GqlClientService } from '@common/libs/gqlclient/gqlclient.service';
 
 @Injectable()
 export class ModuleService {
+  private output: string = `{
+    id
+    name
+    parent_id
+    sub_modules {
+      id
+      name
+      parent_id
+      status
+      created_on
+      created_by
+    }
+    status
+    created_on
+    created_by
+  }`;
   constructor(private readonly gqlClient: GqlClientService) {}
 
   async list(): Promise<Module[]> {
     const params = `query {
-      modules: modulesList {
-        id
-        name
-        parent_id
-        sub_modules {
-          id
-          name
-          parent_id
-          permissions {
-            id
-            record_type
-            created_on
-            created_by
-          }
-          status
-          created_on
-          created_by
-        }
-        permissions {
-          id
-          record_type
-          created_on
-          created_by
-        }
-        status
-        created_on
-        created_by
-      }
+      modules: modulesList ${this.output}
     }`;
     const modules = await this.gqlClient.send(params);
     return modules?.modules;
   }
 
+  async findBy(condition: any, output?: string): Promise<Module[]> {
+    const _output: string = output ? output : this.output;
+    const params = `query {
+      module: findModuleBy(checks: ${toGraphql(condition)}) ${_output}
+    }`;
+    const result = await this.gqlClient.send(params);
+    return result?.module;
+  }
+
   async create(input: ModuleDto): Promise<Module> {
+    const [module] = await this.findBy(
+      [
+        {
+          record_key: 'name',
+          record_value: input.name,
+        },
+      ],
+      `{id}`,
+    );
+    if (module) {
+      throw new BadRequestException(`Module '${input.name}' Already Exist`);
+    }
     const params = `mutation{
-      module: addModule(input: ${toGraphql(input)}) {
-        id
-        name
-        parent_id
-        sub_modules {
-          id
-          name
-          parent_id
-          permissions {
-            id
-            record_type
-            created_on
-            created_by
-          }
-          status
-          created_on
-          created_by
-        }
-        permissions {
-          id
-          record_type
-          created_on
-          created_by
-        }
-        status
-        created_on
-        created_by
-      }
+      module: addModule(input: ${toGraphql(input)}) ${this.output}
     }`;
     const result = await this.gqlClient.send(params);
     return result?.module;
@@ -80,34 +67,7 @@ export class ModuleService {
 
   async findOne(id: string): Promise<Module> {
     const params = `query {
-      module: findModule(id: "${id}") {
-        id
-        name
-        parent_id
-        sub_modules {
-          id
-          name
-          parent_id
-          permissions {
-            id
-            record_type
-            created_on
-            created_by
-          }
-          status
-          created_on
-          created_by
-        }
-        permissions {
-          id
-          record_type
-          created_on
-          created_by
-        }
-        status
-        created_on
-        created_by
-      }
+      module: findModule(id: "${id}") ${this.output}
     }`;
     const result = await this.gqlClient.send(params);
     return result?.module;
@@ -129,34 +89,9 @@ export class ModuleService {
       throw new NotFoundException('Module Not Found');
     }
     const params = `mutation {
-      module: updateModule(id: "${id}", input: ${toGraphql(input)}) {
-        id
-        name
-        parent_id
-        sub_modules {
-          id
-          name
-          parent_id
-          permissions {
-            id
-            record_type
-            created_on
-            created_by
-          }
-          status
-          created_on
-          created_by
-        }
-        permissions {
-          id
-          record_type
-          created_on
-          created_by
-        }
-        status
-        created_on
-        created_by
-      }
+      module: updateModule(id: "${id}", input: ${toGraphql(input)}) ${
+      this.output
+    }
     }`;
     const result = await this.gqlClient.send(params);
     return result?.module;
