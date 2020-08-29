191d30c57d5d4072df6ee7ecf08075c0419d9e39
Faizan AH <faizan@aiondigital.com>
2020-08-29 15:39:57 +0500

    fix: update typo fix in role repository

--- retail/middleware/entitlement.service/src/core/repository/role.repository.ts
+++ retail/middleware/entitlement.service/src/core/repository/role.repository.ts
@@ -1,57 +1,64 @@
-import {Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { BaseRepository } from './base.repository';
-import {STATUS, TABLE,} from '@common/constants';
+import { STATUS, TABLE } from '@common/constants';
 
 @Injectable()
 export class RoleRepository extends BaseRepository {
   private readonly __attributes: string[] = [
-    `${TABLE.MODULE}.id`,
-    `${TABLE.MODULE}.name`,
-    `${TABLE.MODULE}.status`,
-    `${TABLE.MODULE}.description`,
-    `${TABLE.MODULE}.updated_on`,
-    `${TABLE.MODULE}.updated_by`,
-    `${TABLE.MODULE}.deleted_on`,
-    `${TABLE.MODULE}.deleted_by`,
-    `${TABLE.MODULE}.created_by`,
-    `${TABLE.MODULE}.created_on`
+    `${TABLE.ROLE}.id`,
+    `${TABLE.ROLE}.name`,
+    `${TABLE.ROLE}.status`,
+    `${TABLE.ROLE}.description`,
+    `${TABLE.ROLE}.updated_on`,
+    `${TABLE.ROLE}.updated_by`,
+    `${TABLE.ROLE}.deleted_on`,
+    `${TABLE.ROLE}.deleted_by`,
+    `${TABLE.ROLE}.created_by`,
+    `${TABLE.ROLE}.created_on`,
   ];
 
   constructor() {
     super(TABLE.ROLE);
   }
 
-  async listRolesByUserID(userIds): Promise<any>{
+  async listRolesByUserID(userIds): Promise<any> {
     const condition = {};
     condition[`${TABLE.ROLE}.status`] = STATUS.ACTIVE;
     return this._connection(TABLE.ROLE)
-        .select([...this.__attributes, `${TABLE.USER_ROLE}.user_id`])
-        .leftJoin(TABLE.USER_ROLE, `${TABLE.ROLE}.id`, `${TABLE.USER_ROLE}.role_id`)
-        .whereIn(`${TABLE.USER_ROLE}.user_id`, userIds)
-        .where(condition);
+      .select([...this.__attributes, `${TABLE.USER_ROLE}.user_id`])
+      .leftJoin(
+        TABLE.USER_ROLE,
+        `${TABLE.ROLE}.id`,
+        `${TABLE.USER_ROLE}.role_id`,
+      )
+      .whereIn(`${TABLE.USER_ROLE}.user_id`, userIds)
+      .where(condition);
   }
 
   async create(role: Record<string, any>, keys: string[]): Promise<any> {
     const trxProvider = this._connection.transactionProvider();
     const trx = await trxProvider();
-    try{
+    try {
       const permissions = role.permissions;
       delete role.permissions;
       const response = await trx(TABLE.ROLE).insert(role, keys);
-      if(permissions) {
+      if (permissions) {
         const module_permission_roles = permissions.map(module_permission => {
           return {
-            role_id : response[0].id || response[0],
-            module_permission_id : module_permission['id'],
+            role_id: response[0].id || response[0],
+            module_permission_id: module_permission['id'],
           };
         });
-        await trx(TABLE.MODULE_PERMISSION_ROLE).insert(module_permission_roles, ['module_permission_id']);
+        await trx(TABLE.MODULE_PERMISSION_ROLE).insert(
+          module_permission_roles,
+          ['module_permission_id'],
+        );
       }
       await trx.commit();
-      return response
+      return response;
     } catch (e) {
       await trx.rollback();
-      throw e
+      throw e;
     }
   }
 }
