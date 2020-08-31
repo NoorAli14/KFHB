ac90aa2927b17a6fdce012c72c0f228ac202f482
Faizan AH <faizan@aiondigital.com>
2020-08-31 14:04:13 +0500

    fix: [APS-475] fix roles services

--- retail/middleware/entitlement.service/src/core/repository/role.repository.ts
+++ retail/middleware/entitlement.service/src/core/repository/role.repository.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { BaseRepository } from './base.repository';
-import {STATUS, TABLE} from '@common/constants';
-import {IdsInput} from '@common/inputs/ids.input';
+import { STATUS, TABLE } from '@common/constants';
+import { IdsInput } from '@common/inputs/ids.input';
 
 @Injectable()
 export class RoleRepository extends BaseRepository {
@@ -15,7 +15,7 @@ export class RoleRepository extends BaseRepository {
     `${TABLE.ROLE}.deleted_on`,
     `${TABLE.ROLE}.deleted_by`,
     `${TABLE.ROLE}.created_by`,
-    `${TABLE.ROLE}.created_on`
+    `${TABLE.ROLE}.created_on`,
   ];
 
   constructor() {
@@ -36,38 +36,47 @@ export class RoleRepository extends BaseRepository {
       .where(condition);
   }
 
-  async update(condition: Record<string, any>,
-               role: Record<string, any>,
-               keys: string[]): Promise<any> {
+  async update(
+    condition: Record<string, any>,
+    role: Record<string, any>,
+    keys: string[],
+  ): Promise<any> {
     const trxProvider = this._connection.transactionProvider();
     const trx = await trxProvider();
-    try{
+    try {
       const module_permission_ids: IdsInput[] = role.permissions;
       delete role.permissions;
-      const response = await trx(TABLE.ROLE).where(condition).update(role, keys);
-      if(module_permission_ids.length > 0) {
+      const response = await trx(TABLE.ROLE)
+        .where(condition)
+        .update(role, keys);
+      if (module_permission_ids?.length > 0) {
         // deleting role and module-permission relations
-        const modulePermissionsToDelete = module_permission_ids.filter(module_permission_id => module_permission_id._deleted)
+        const modulePermissionsToDelete = module_permission_ids
+          .filter(module_permission_id => module_permission_id._deleted)
           .map(module_permission_id => module_permission_id.id);
         await trx(TABLE.MODULE_PERMISSION_ROLE)
           .whereIn('module_permission_id', modulePermissionsToDelete)
-          .where({role_id: response[0].id || response[0]})
+          .where({ role_id: response[0].id || response[0] })
           .del();
         //creating role and module-permission relations
-        const module_permission_roles = module_permission_ids.filter(module_permission_id => !module_permission_id._deleted)
+        const module_permission_roles = module_permission_ids
+          .filter(module_permission_id => !module_permission_id._deleted)
           .map(module_permission_id => {
             return {
-              role_id : response[0].id || response[0],
-              module_permission_id : module_permission_id.id,
+              role_id: response[0].id || response[0],
+              module_permission_id: module_permission_id.id,
             };
-        });
-        if(module_permission_roles.length > 0) await trx(TABLE.MODULE_PERMISSION_ROLE).insert(module_permission_roles, ['id']);
+          });
+        if (module_permission_roles?.length > 0)
+          await trx(
+            TABLE.MODULE_PERMISSION_ROLE,
+          ).insert(module_permission_roles, ['module_permission_id']);
       }
       await trx.commit();
-      return response
+      return response;
     } catch (e) {
       await trx.rollback();
-      throw e
+      throw e;
     }
   }
 
