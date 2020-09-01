c323aa2a6a7f8ca1dcd85148d8f7cd306ae7a46d
Faizan AH <faizan@aiondigital.com>
2020-09-01 21:26:01 +0500

    fix: [APS-475] update role update fix

--- retail/middleware/entitlement.business.service/src/app/v1/roles/roles.service.ts
+++ retail/middleware/entitlement.business.service/src/app/v1/roles/roles.service.ts
@@ -72,6 +72,7 @@ export class RoleService {
     if (!role) {
       throw new NotFoundException('Role Not Found');
     }
+
     const params = `mutation {
       result: updateRole(id: "${id}", input: ${toGraphql(input)}) ${this.output}
     }`;
