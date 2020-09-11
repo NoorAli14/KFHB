92194fd2a441b907780135d6f02226c106fd9779
Fatema Zainaldin <fatema@aiondigital.com>
2020-09-11 10:27:21 +0300

    1. Adding custom theme 2. Applying custom theme 3. changing logo

--- retail/webadmin/src/app/features/referrals/components/referrals.component.ts
+++ retail/webadmin/src/app/features/referrals/components/referrals.component.ts
@@ -48,23 +48,17 @@ export class ReferralsComponent extends BaseComponent implements OnInit {
     'Referring Customer email',
     'Referring customer mobile no.'
   ];
-
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
-
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
-
   constructor(public _matDialog: MatDialog) {
     super('Referrals');
   }
-
   ngOnInit(): void {
     super.ngOnInit();
     this.username = new FormControl('');
     this.initSearch();
   }
-
-
   initSearch(): void {
     this.username.valueChanges.subscribe((text: string) => {
     });
@@ -75,24 +69,6 @@ export class ReferralsComponent extends BaseComponent implements OnInit {
   camelToSentenceCase(text): void {
     return camelToSentenceCase(text);
   }
-  confirmDialog(type, id): void {
-    const message = type === 'invite' ? removeRandom(MESSAGES.RESEND_INVITE()) : removeRandom(MESSAGES.REMOVE_CONFIRMATION());
-    const dialogData = new ConfirmDialogModel('Confirm Action', message);
-    const dialogRef = this._matDialog.open(ConfirmDialogComponent, {
-      data: dialogData,
-      disableClose: true,
-      panelClass: 'app-confirm-dialog',
-      hasBackdrop: true,
-    });
-
-    dialogRef.afterClosed().subscribe((status) => {
-      if (status) {
-     //   type === 'invite' ? this.resendInvitation(id) : this.deleteUser(id)
-      }
-    });
-  }
-
-
   updateGrid(data): void {
     this.dataSource.data = data;
     this.dataSource.paginator = this.paginator;
