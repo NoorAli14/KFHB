66c7b201c5f44204d438fda2c2fdfdce40c3909f
Sajid Ali <sajid@aiondigital.com>
2020-09-12 22:48:47 +0500

    added migration script

---
+++ retail/middleware/template.service/src/core/database/migrations/20200908191218_cmp_aml_request.ts
@@ -0,0 +1,29 @@
+import * as Knex from 'knex';
+import { TABLE, DATABASE_UUID_METHOD } from '@common/constants';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE.AML_REQUEST, table => {
+    table
+      .uuid('id')
+      .primary()
+      .defaultTo(knex.raw(DATABASE_UUID_METHOD));
+
+    table.uuid('customer_id').notNullable();
+    table.string('aml_text');
+    table.string('remarks');
+    table.string('status');
+
+    table.timestamp('created_on').defaultTo(knex.fn.now());
+    table.string('created_by');
+
+    table.timestamp('updated_on').defaultTo(knex.fn.now());
+    table.string('updated_by');
+
+    table.timestamp('deleted_on');
+    table.string('deleted_by');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(TABLE.AML_REQUEST);
+}
---
+++ retail/middleware/template.service/src/core/database/migrations/20200908191238_cmp_aml_response.ts
@@ -0,0 +1,34 @@
+import * as Knex from 'knex';
+import { TABLE, DATABASE_UUID_METHOD } from '@common/constants';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE.AML_RESPONSE, table => {
+    table
+      .uuid('id')
+      .primary()
+      .defaultTo(knex.raw(DATABASE_UUID_METHOD));
+    table.string('request_reference');
+    table.string('response_status');
+
+    table.timestamp('response_on');
+    table.integer('response_type');
+
+    table.timestamp('created_on').defaultTo(knex.fn.now());
+    table.string('created_by');
+
+    table.timestamp('updated_on').defaultTo(knex.fn.now());
+    table.string('updated_by');
+
+    table.timestamp('deleted_on');
+    table.string('deleted_by');
+    table.uuid('request_id');
+    table
+      .foreign('request_id')
+      .references('id')
+      .inTable(TABLE.AML_REQUEST);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(TABLE.AML_RESPONSE);
+}
