7376be9107b9c400049caac19396b886cb0822a7
Rashid Aslam <rashidaslam75@gmail.com>
2020-09-15 14:45:39 +0500

    added code coverage

--- retail/webadmin/src/app/shared/components/required-indicator/required-indicator.component.ts
+++ retail/webadmin/src/app/shared/components/required-indicator/required-indicator.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-required-indicator',
-  template: `<span>*</span>`
+  template: `<span style="color:red">*</span>`
 })
 export class RequiredIndicatorComponent implements OnInit {
 
