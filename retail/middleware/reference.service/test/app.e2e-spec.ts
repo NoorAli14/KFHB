2b33e3629041c19a361f84daec247bba6f919362
Muhammad Zaid <mzaid@aiondigital.com>
2020-09-17 11:46:55 +0500

    feat: added generic application tests

--- retail/middleware/reference.service/test/app.e2e-spec.ts
+++ retail/middleware/reference.service/test/app.e2e-spec.ts
@@ -1,24 +1,32 @@
+import request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '@app/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let server: any;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    server = app.getHttpServer();
+    // const router = server._events.request._router;
+    // console.log('API:', router);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
+  it('/GET info', (done) => {
+    return request(server)
+      .get('/info')
       .expect(200)
-      .expect('Hello World!');
+      .end(done);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
