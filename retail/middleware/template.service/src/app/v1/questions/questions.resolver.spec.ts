d50befa0f1a9089390cd6be338da6223d3a02539
Sajid Ali <sajid@aiondigital.com>
2020-09-22 11:57:43 +0500

    test case for resolver questions

--- retail/middleware/template.service/src/app/v1/questions/questions.resolver.spec.ts
+++ retail/middleware/template.service/src/app/v1/questions/questions.resolver.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuestionsResolver } from './questions.resolver';
+import { QuestionsService } from './questions.service';
 
 describe('QuestionsResolver', () => {
   let resolver: QuestionsResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QuestionsResolver],
+      providers: [
+        QuestionsResolver,
+        {
+          provide: QuestionsService,
+          useFactory: () => ({
+            findQuestion: jest.fn((id: string) => ({
+              id: id,
+              title: 'test question',
+              title_ar: 'test question ar',
+              rules: 'test rules',
+              status: 'test status',
+              type: 'test type',
+            })),
+          }),
+        },
+      ],
     }).compile();
 
     resolver = module.get<QuestionsResolver>(QuestionsResolver);
@@ -14,5 +30,16 @@ describe('QuestionsResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('findQuestion', () => {
+    expect(resolver.findQuestion('123-123-123', [])).toEqual({
+      id: '123-123-123',
+      title: 'test question',
+      title_ar: 'test question ar',
+      rules: 'test rules',
+      status: 'test status',
+      type: 'test type',
+    });
   });
 });
