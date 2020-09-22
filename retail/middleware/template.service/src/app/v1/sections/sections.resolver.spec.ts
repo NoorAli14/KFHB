b9c4822d68c9a67788bdc6f8f21ef16a13ac8230
Sajid Ali <sajid@aiondigital.com>
2020-09-22 12:08:30 +0500

    test case for complince module

--- retail/middleware/template.service/src/app/v1/sections/sections.resolver.spec.ts
+++ retail/middleware/template.service/src/app/v1/sections/sections.resolver.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SectionsResolver } from './sections.resolver';
+import { SectionsService } from './sections.service';
 
 describe('SectionsResolver', () => {
   let resolver: SectionsResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SectionsResolver],
+      providers: [
+        SectionsResolver,
+        {
+          provide: SectionsService,
+          useFactory: () => ({
+            findSection: jest.fn((id: string) => ({
+              id: id,
+              name: 'sextion name',
+              name_ar: 'section name ar',
+              level: 'test level',
+            })),
+          }),
+        },
+      ],
     }).compile();
 
     resolver = module.get<SectionsResolver>(SectionsResolver);
@@ -14,5 +28,14 @@ describe('SectionsResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('findSection', () => {
+    expect(resolver.findSection('123-123-123', [])).toEqual({
+      id: '123-123-123',
+      name: 'sextion name',
+      name_ar: 'section name ar',
+      level: 'test level',
+    });
   });
 });
--- retail/middleware/template.service/src/app/v1/template-questions/template-questions.resolver.spec.ts
+++ retail/middleware/template.service/src/app/v1/template-questions/template-questions.resolver.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TemplateQuestionsResolver } from './template-questions.resolver';
+import { TemplateQuestionsService } from './template-questions.service';
 
 describe('TemplateQuestionsResolver', () => {
   let resolver: TemplateQuestionsResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TemplateQuestionsResolver],
+      providers: [
+        TemplateQuestionsResolver,
+        {
+          provide: TemplateQuestionsService,
+          useFactory: () => ({
+            findTemplateQuestion: jest.fn((id: string) => ({
+              id: id,
+              role: 'test role',
+              status: false,
+            })),
+          }),
+        },
+      ],
     }).compile();
 
     resolver = module.get<TemplateQuestionsResolver>(TemplateQuestionsResolver);
@@ -14,5 +27,13 @@ describe('TemplateQuestionsResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('findTemplateQuestion', () => {
+    expect(resolver.findTemplateQuestion('123-123-123', [])).toEqual({
+      id: '123-123-123',
+      role: 'test role',
+      status: false,
+    });
   });
 });
