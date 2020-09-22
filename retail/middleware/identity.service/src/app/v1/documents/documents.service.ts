5ce6d15767f6916b8811c51e6fb2ea89c0b60bd1
Faizan AH <faizan@aiondigital.com>
2020-09-22 06:18:26 +0500

    feat: [APS-478] enable dynamic schema validation in document processing module

--- retail/middleware/identity.service/src/app/v1/documents/documents.service.ts
+++ retail/middleware/identity.service/src/app/v1/documents/documents.service.ts
@@ -1,17 +1,20 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import {
   SessionRepository,
   CustomerRepository,
   DocumentTypeRepository,
   SessionReferenceRepository,
 } from '@rubix/core';
-import { Document, PreviewDocument } from './document.model';
+import { CUSTOM_ERROR, Document, PreviewDocument, SCHEMA_ERROR } from './document.model';
 import { IdentityService } from '@rubix/common/connectors';
 import {
   DOCUMENT_STATUSES,
   ICurrentUser,
   DOCUMENT_TYPES,
 } from '@rubix/common/';
+import { SchemaService, ISCHEMA_ERROR } from './schema.service';
+import { SessionNotFoundException } from '@app/v1/sessions/exceptions/';
+import { DocumentNotFoundException } from './exceptions/';
 @Injectable()
 export class DocumentsService {
   private readonly logger: Logger = new Logger(DocumentsService.name);
@@ -21,44 +24,48 @@ export class DocumentsService {
     private readonly sessionReferenceDB: SessionReferenceRepository,
     private readonly sessionDB: SessionRepository,
     private readonly customerDB: CustomerRepository,
-  ) {}
+    private readonly schema: SchemaService
+  ) { }
 
   async create(
     currentUser: ICurrentUser,
     input: { [key: string]: any },
     columns?: string[],
   ): Promise<Document> {
+    const [document, customerSession] = await Promise.all([
+      this.documentTypeDB.findByName(input.type),
+      this.customerDB.getRecentSession(currentUser.tenant_id, currentUser.id),
+    ]);
+    if (!document)
+      throw new DocumentNotFoundException();
+    if (!customerSession)
+      throw new SessionNotFoundException();
+    const identityDocument: any = await this.identityService.createDocument(
+      customerSession.target_user_id,
+      customerSession.check_id,
+      {
+        file: input.file,
+        unprocessedImage: input.unprocessed,
+      },
+    );
     const trx: any = await this.customerDB.transaction();
     try {
-      const promises = await Promise.all([
-        this.documentTypeDB.findByName(input.type),
-        this.customerDB.getRecentSession(currentUser.tenant_id, currentUser.id),
-      ]);
-      const customerSession: any = promises[1];
-      const identityDocument: any = await this.identityService.createDocument(
-        customerSession.target_user_id,
-        customerSession.check_id,
-        {
-          file: input.file,
-          unprocessedImage: input.unprocessed,
-        },
-      );
       const params: { [key: string]: string } = {
         tenant_id: currentUser.tenant_id,
         session_id: customerSession.session_id,
-        document_type_id: promises[0].id,
+        document_type_id: document.id,
         status: DOCUMENT_STATUSES.PROCESSING,
         attachable_id: identityDocument.id,
         created_by: currentUser.id,
         updated_by: currentUser.id,
       };
-      const [document] = await this.sessionReferenceDB.create(
+      const [sessionRef] = await this.sessionReferenceDB.create(
         params,
         columns,
         trx,
       );
       await trx.commit();
-      return document;
+      return sessionRef;
     } catch (error) {
       await trx.rollback();
       throw error;
@@ -68,21 +75,27 @@ export class DocumentsService {
   async process(
     currentUser: ICurrentUser,
     input: { [key: string]: any },
-    output?: string[],
-  ): Promise<Document> {
+    output: string[],
+  ): Promise<Document | CUSTOM_ERROR> {
+
     //  Fetch customer recent active session
     const customerSession = await this.customerDB.getRecentSession(
       currentUser.tenant_id,
       currentUser.id,
     );
+    if (!customerSession) {
+      this.logger.log(`Session not exit with customer ID [${currentUser.id}]`)
+      throw new SessionNotFoundException()
+    }
     const [reference]: any = await this.sessionReferenceDB.recentDocumentByType(
       customerSession.session_id,
       input.type,
     );
 
-    this.logger.log(`${JSON.stringify(customerSession, null, 2)}`);
-    this.logger.log(`${JSON.stringify(reference, null, 2)}`);
-
+    if (!reference) {
+      this.logger.log(`Document not exit with customer ID [${currentUser.id}]`)
+      throw new DocumentNotFoundException();
+    }
     if (reference?.status === DOCUMENT_STATUSES.PROCESSING) {
       const status: any = await this.identityService.getDocument(
         reference.target_user_id,
@@ -94,8 +107,9 @@ export class DocumentsService {
         status:
           DOCUMENT_STATUSES[status?.processingStatus] ||
           DOCUMENT_STATUSES.FAILED,
+        processed_data: null
       };
-
+      let result: CUSTOM_ERROR = { valid: true, errors: null }
       if (status?.processingStatus === DOCUMENT_STATUSES.PROCESSED) {
         const processedData: any = await this.identityService.getServerProcessedOCRSensitiveData(
           reference.target_user_id,
@@ -108,6 +122,10 @@ export class DocumentsService {
             visualZone: this.identityService.sanitize(processedData.visualZone),
           };
           _input.processed_data = JSON.stringify(sanitizeData);
+          result = this.schema.validate(input.type, sanitizeData?.mrz);
+          if (!result.valid) {
+            _input.status = DOCUMENT_STATUSES.FAILED;
+          }
         }
       }
       const [sessionRef] = await this.sessionReferenceDB.update(
@@ -115,10 +133,18 @@ export class DocumentsService {
         _input,
         output,
       );
+      if (!result.valid) {
+        return result;
+      }
       return sessionRef;
-    } else {
-      return reference;
+    } else if (reference?.status === DOCUMENT_STATUSES.FAILED && reference?.processed_data) {
+      const data = JSON.parse(reference.processed_data)
+      const result = this.schema.validate(input.type, data.mrz);
+      if (!result.valid) {
+        return result;
+      }
     }
+    return reference;
   }
 
   async preview(input: any): Promise<PreviewDocument> {
