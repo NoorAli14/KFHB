42630cc8f4d5f4ecdd2cdacbb680a94365affeb7
Faizan AH <faizan@aiondigital.com>
2020-09-26 15:02:15 +0500

    fix: [APS-478] update compliance service

--- retail/middleware/compliance.service/src/app/v1/template-responses/template-response.dto.ts
+++ retail/middleware/compliance.service/src/app/v1/template-responses/template-response.dto.ts
@@ -1,18 +1,23 @@
-import { MaxLength } from 'class-validator';
+import { IsString, IsNotEmpty, IsUUID, IsOptional } from 'class-validator';
 import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class NewTemplateResponseInput {
   @Field()
-  @MaxLength(1000)
+  @IsString()
+  @IsNotEmpty()
   results: string;
 
   @Field()
+  @IsString()
+  @IsOptional()
   remarks: string;
 
   @Field()
+  @IsUUID()
   template_id: string;
 
   @Field()
+  @IsUUID()
   user_id: string;
 }
