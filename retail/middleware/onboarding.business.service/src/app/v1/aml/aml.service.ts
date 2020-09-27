c66b40f27291398c2d2fc2a396fb7d4c328efbf6
Faizan AH <faizan@aiondigital.com>
2020-09-27 18:45:12 +0500

    feat: [APS-478] added aml module

---
+++ retail/middleware/onboarding.business.service/src/app/v1/aml/aml.service.ts
@@ -0,0 +1,28 @@
+
+import { Injectable, Logger } from '@nestjs/common';
+import { GqlClientService } from '@common/index';
+import { Aml } from './aml.entity';
+
+@Injectable()
+export class AmlService {
+  private readonly logger: Logger = new Logger(AmlService.name);
+  private readonly output: string = `{
+    id
+    request_reference
+    status
+    created_by
+    created_on
+    updated_on
+    updated_by
+  }`;
+
+  constructor(private readonly gqlClient: GqlClientService) { }
+
+  async screening(user_id: string): Promise<Aml> {
+    this.logger.log(`Aml:: Start aml screening for customer [${user_id}]`)
+    const mutation: string = `mutation {
+      result: amlScreening(user_id: "${user_id}") ${this.output}
+    }`;
+    return this.gqlClient.send(mutation);
+  }
+}
