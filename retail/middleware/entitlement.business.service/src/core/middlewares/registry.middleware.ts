bbaa25f9a2dc08cf4dd4c61ac60a4792a5f6d6f3
Faizan AH <faizan@aiondigital.com>
2020-09-28 14:21:55 +0500

    fix: [APS-531] update gqlclient and service registry middleware

--- retail/middleware/entitlement.business.service/src/core/middlewares/registry.middleware.ts
+++ retail/middleware/entitlement.business.service/src/core/middlewares/registry.middleware.ts
@@ -1,19 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import { UnauthorizedException } from '@nestjs/common';
 import { X_CHANNEL_KEY } from '@common/constants';
-import { SERVICES } from '@volume/registry'
+import { SERVICES } from '@volume/registry.service';
 
-const isAuthorizedChannel = (url, channelName) => {
+const isAuthorizedChannel = (url: string, channelName: string): boolean => {
   const service = SERVICES[url];
   return service?.channels?.includes(channelName);
 }
 
 export const RegistryMiddleware = (): any => {
   return (req: Request, res: Response, next: NextFunction): void => {
-    const channelId = req.headers[X_CHANNEL_KEY];
-    const urlPrefix = req.originalUrl.split("/").slice(1, 5).join("/");
-    console.log("urlPrefix", urlPrefix)
-    if(isAuthorizedChannel(urlPrefix, channelId)) {
+    const channelId: string = req.headers[X_CHANNEL_KEY] as string;
+    console.log(`RegistryMiddleware:: Initialize registry middleware [${channelId}]`);
+    const urlPrefix: string = req.originalUrl?.split("/")?.slice(1, 5)?.join("/");
+    if (urlPrefix === 'graphql' || isAuthorizedChannel(urlPrefix, channelId)) {
       return next();
     }
     next(new UnauthorizedException('Unauthorized Channel'));
