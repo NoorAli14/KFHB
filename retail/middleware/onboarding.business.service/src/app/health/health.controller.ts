42e8b9f2d557b897b9248cae497049dc5b941081
Faizan AH <faizan@aiondigital.com>
2020-10-04 15:54:25 +0500

    feat: [APS-478] updated health container

--- retail/middleware/onboarding.business.service/src/app/health/health.controller.ts
+++ retail/middleware/onboarding.business.service/src/app/health/health.controller.ts
@@ -7,8 +7,9 @@ import {
 } from '@nestjs/terminus';
 import { Controller, Get } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { RegistryService } from '@common/services/registry.service';
 
-@ApiTags('Health Checking')
+@ApiTags('Health')
 @Controller('health')
 export class HealthController {
   constructor(
@@ -16,36 +17,20 @@ export class HealthController {
     private readonly dns: DNSHealthIndicator,
     private readonly memory: MemoryHealthIndicator,
     private readonly disk: DiskHealthIndicator,
-  ) {}
+    private readonly registry: RegistryService
+  ) { }
 
   @Get()
   @HealthCheck()
   @ApiResponse({ status: 200, description: 'Health status of the service' })
   healthCheck(): Promise<any> {
+    const promises = [async (): Promise<any> => this.memory.checkRSS('memory_rss', 3000 * 1024 * 1024), async (): Promise<any> => this.memory.checkHeap('memory_heap', 200 * 1024 * 1024)]
+    const _promises = this.registry.services.map(service => {
+      const host_name: string = `${service.is_secure ? 'https' : 'http'}://${service.host_name}:${service.port
+        }/api/v1/health`;
+      return async (): Promise<any> => this.dns.pingCheck(service.name, host_name)
+    })
     //Health indicator executes a check of a service, whether it is in a healthy or unhealthy state
-    return this.health.check([
-      async (): Promise<any> =>
-        /* Author: Faizan AH
-         * Date  : 07/25/2020
-         * Note  : We need add a ping here of our all required services. It should be dynamic later through service registry.
-         */
-        /* DNS health Checker.
-         * @params: SERVICE_NAME | example: google | customer | accounts
-         * @params: SERVICE_HOST | example: https://google.com | https://customer:4001 | https://accounts:4006
-         */
-        this.dns.pingCheck('google', 'https://google.com'),
-      async (): Promise<any> =>
-        // RSS Memory health Checker.
-        this.memory.checkRSS('memory_rss', 3000 * 1024 * 1024),
-      async (): Promise<any> =>
-        // Heap Memory health Checker.
-        this.memory.checkHeap('memory_heap', 200 * 1024 * 1024),
-      // async (): Promise<any> =>
-      // // Disk Space stoarge Checker.
-      //   this.disk.checkStorage('storage', {
-      //     threshold: 2 * 1024 * 1024 * 1024,
-      //     path: '/',
-      //   }),
-    ]);
+    return this.health.check([..._promises, ...promises]);
   }
 }
