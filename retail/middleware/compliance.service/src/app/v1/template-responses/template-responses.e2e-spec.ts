f510a935735e556fb82ce71d6ebae50c11982fc3
Sajid Ali <sajid@aiondigital.com>
2020-10-07 11:28:34 +0500

    added test cases, for template response

--- retail/middleware/compliance.service/src/app/v1/template-responses/template-responses.e2e-spec.ts
+++ retail/middleware/compliance.service/src/app/v1/template-responses/template-responses.e2e-spec.ts
@@ -10,8 +10,8 @@ describe('Complince Module (e2e)', () => {
   const templateResponses: any = {
     results: 'Test response results',
     remarks: 'Test response remarks',
-    tenant_id: '123-121',
-    user_id: '123-121',
+    template_id: '37678c69-dde5-4452-a66b-401f32211427',
+    user_id: '828605C2-7E50-40BC-AA88-C064CE63C155',
   };
   const headers: { [key: string]: any } = {
     'x-tenant-id': '9013C327-1190-4875-A92A-83ACA9029160',
@@ -32,11 +32,20 @@ describe('Complince Module (e2e)', () => {
     return request(app.getHttpServer())
       .post('/graphql')
       .send({
-        query: `{findTemplateResponseByUserId{id status remaks}}`,
+        query: `{findTemplateResponseByUserId(user_id: "828605C2-7E50-40BC-AA88-C064CE63C155"){id results status remarks user_id template {id name}}}`,
       })
       .set(headers)
       .expect(({ body }) => {
-        // const data = body.data.findTemplateResponseByUserId;
+        const data = body?.data?.findTemplateResponseByUserId;
+        expect(data).toBeInstanceOf(Array);
+        expect(data).toHaveLength(data.length);
+        expect(
+          data
+            .map((response: any) => response.user_id)
+            .includes(
+              data[0].user_id || '828605C2-7E50-40BC-AA88-C064CE63C155',
+            ),
+        ).toBeTruthy();
       })
       .expect(200)
       .end(done);
