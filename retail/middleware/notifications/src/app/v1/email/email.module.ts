0ede24aede90abd6bab69fa9abc71ee727b7fef6
Faizan AH <faizan@aiondigital.com>
2020-10-10 19:11:55 +0500

    docs: [APS-478] update templates path

--- retail/middleware/notifications/src/app/v1/email/email.module.ts
+++ retail/middleware/notifications/src/app/v1/email/email.module.ts
@@ -20,7 +20,7 @@ import { ConfigurationService } from '@common/configuration/configuration.servic
         preview: false,
         template: {
           // dir: __dirname + '/templates',
-          dir: join(`${process.cwd()}`, './dist/src/app/v1/email/templates'),
+          dir: join(`${process.cwd()}`, './volumes/templates'),
           adapter: new PugAdapter(),
           options: {
             strict: true,
