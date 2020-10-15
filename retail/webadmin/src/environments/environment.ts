6df10e2ebc5b4d52c05a401847fa196a82d94fee
FatemaAhmedZainaldin <fatema@aiondigital.com>
2020-10-15 16:18:43 +0300

    updating constant in environment.prod

--- retail/webadmin/src/environments/environment.ts
+++ retail/webadmin/src/environments/environment.ts
@@ -10,7 +10,6 @@ export const environment = {
     isMockEnabled: true, // You have to switch this, when your real back-end is done
     authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
     APP_SECRETE: 'abfFGHIepqrstuvwSTUCDEXYZ',
-    // API_BASE_URL:"http://localhost:3000",
     API_BASE_URL: 'https://rubix-dev01.conduit-aiondigital.com',
     API_BASE_URL_2: 'https://aionj2middlewaredev.westindia.cloudapp.azure.com:8080',
     CHANNEL_ID: 'RUBIX-DEV-INT',
