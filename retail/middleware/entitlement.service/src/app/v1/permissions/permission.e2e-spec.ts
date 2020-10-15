937c2037afa1302499524226a9c58dd3d725d2d4
Saeed Raza <sraza@aiondigital.com>
2020-10-15 07:14:25 +0500

    refactored: [APS-475] added class-transformer-validator in permissions test cases

--- retail/middleware/entitlement.service/src/app/v1/permissions/permission.e2e-spec.ts
+++ retail/middleware/entitlement.service/src/app/v1/permissions/permission.e2e-spec.ts
@@ -1,10 +1,13 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import {transformAndValidateSync} from "class-transformer-validator";
+
 import {getChecksQuery, getDeleteMutation, getMutation, getQuery} from '@common/tests';
 import {V1Module} from '@app/v1/v1.module';
 import {PermissionInput} from '@app/v1/permissions/permission.dto';
 import {KeyValInput} from '@common/inputs/key-val.input';
+import {Permission} from "@app/v1/permissions/permission.model";
 
 
 describe('Permission Module (e2e)', () => {
@@ -22,20 +25,30 @@ describe('Permission Module (e2e)', () => {
 
   it(`adds Permission`, done => {
     const input: PermissionInput = {
-      record_type: "E2E-testing"
+      record_type: "permission_e2e_testing"
     };
+    const permission_validated: Permission = transformAndValidateSync(
+        Permission,
+        input,
+    ) as Permission;
     return request(app.getHttpServer())
       .post('/graphql')
       .send({
-        query: getMutation("addPermission", input, "id"),
+        query: getMutation("addPermission", permission_validated, "id"),
       }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
       .expect(( {body} ) => {
-        const data = body.data.addPermission;
-        permissionId = data.id;
-        expect(data.id).toBeDefined();
+        const data = body?.data?.addPermission;
+        permissionId = data?.id;
+        const permission_json: string = JSON.stringify(data);
+        const permission_validated: Permission = transformAndValidateSync(
+            Permission,
+            permission_json,
+        ) as Permission;
+        expect(permission_validated).toBeDefined();
+        expect(permission_validated).toBeInstanceOf(Permission);
       })
       .expect(200)
       .end(done);
@@ -43,19 +56,30 @@ describe('Permission Module (e2e)', () => {
 
   it(`updates Permission`, done => {
     const input: PermissionInput = {
-      record_type: "E2E-updated"
+      record_type: "permission_e2e_testing_updated"
     };
+    const permission_validated: Permission = transformAndValidateSync(
+        Permission,
+        input,
+    ) as Permission;
     return request(app.getHttpServer())
     .post('/graphql')
     .send({
-      query: getMutation("updatePermission", input, "id record_type", permissionId),
+      query: getMutation("updatePermission", permission_validated, "id record_type", permissionId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.updatePermission;
-      expect(data.record_type).toBe("E2E-updated");
+      const data = body?.data?.updatePermission;
+      expect(data?.record_type).toBe("permission_e2e_testing_updated");
+      const permission_json: string = JSON.stringify(data);
+      const permission_validated: Permission = transformAndValidateSync(
+          Permission,
+          permission_json,
+      ) as Permission;
+      expect(permission_validated).toBeDefined();
+      expect(permission_validated).toBeInstanceOf(Permission);
     })
     .expect(200)
     .end(done);
@@ -67,12 +91,19 @@ describe('Permission Module (e2e)', () => {
     .send({
       query: getQuery("permissionsList", "id record_type"),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.permissionsList;
-      expect(data.length).toBeDefined();
+      const data = body?.data?.permissionsList;
+      expect(data?.length).toBeDefined();
+      const permission_json: string = JSON.stringify(data);
+      const permission_validated: Permission[] = transformAndValidateSync(
+          Permission,
+          permission_json,
+      ) as Permission[];
+      expect(permission_validated).toBeDefined();
+      expect(permission_validated).toBeInstanceOf(Array);
     })
     .expect(200)
     .end(done);
@@ -84,12 +115,18 @@ describe('Permission Module (e2e)', () => {
     .send({
       query: getQuery("findPermissionById", "id record_type", permissionId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.findPermissionById;
-      expect(data.id).toBeDefined();
+      const data = body?.data?.findPermissionById;
+      const permission_json: string = JSON.stringify(data);
+      const permission_validated: Permission = transformAndValidateSync(
+          Permission,
+          permission_json,
+      ) as Permission;
+      expect(permission_validated).toBeDefined();
+      expect(permission_validated).toBeInstanceOf(Permission);
     })
     .expect(200)
     .end(done);
@@ -97,19 +134,26 @@ describe('Permission Module (e2e)', () => {
 
   it(`searches Permissions by properties`, done => {
     const checks: KeyValInput[] = [
-      {record_key: "record_type", record_value: "E2E-testing"}
+      {record_key: "record_type", record_value: "permission_e2e_testing_updated"}
     ];
     return request(app.getHttpServer())
     .post('/graphql')
     .send({
       query: getChecksQuery("findPermissionBy", checks, "id record_type"),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.findPermissionBy;
-      expect(data.length).toBeDefined();
+      const data = body?.data?.findPermissionBy;
+      expect(data?.length).toBeDefined();
+      const permission_json: string = JSON.stringify(data);
+      const permission_validated: Permission[] = transformAndValidateSync(
+          Permission,
+          permission_json,
+      ) as Permission[];
+      expect(permission_validated).toBeDefined();
+      expect(permission_validated).toBeInstanceOf(Array);
     })
     .expect(200)
     .end(done);
@@ -121,11 +165,11 @@ describe('Permission Module (e2e)', () => {
     .send({
       query: getDeleteMutation("deletePermission", permissionId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.deletePermission;
+      const data = body?.data?.deletePermission;
       expect(data).toBeTruthy();
     })
     .expect(200)
@@ -138,12 +182,12 @@ describe('Permission Module (e2e)', () => {
     .send({
       query: getQuery("findPermissionById", "id record_type", permissionId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const [error] = body.errors;
-      expect(error.message).toBe("Permission Not Found");
+      const [error] = body?.errors;
+      expect(error?.message).toBe("Permission Not Found");
     })
     .expect(200)
     .end(done);
--- retail/middleware/entitlement.service/src/app/v1/permissions/permission.model.ts
+++ retail/middleware/entitlement.service/src/app/v1/permissions/permission.model.ts
@@ -1,21 +1,35 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { PaginationModel } from '@common/models';
+import {IsOptional, IsString, IsUUID} from "class-validator";
 
 @ObjectType()
 export class Permission {
   @Field(() => ID, { nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   id?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   record_type?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   created_on?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   created_by?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   module_permission_id?: string;
 }
 
