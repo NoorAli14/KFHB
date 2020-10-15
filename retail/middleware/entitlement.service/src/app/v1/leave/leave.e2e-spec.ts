b768818f776f256116eb90a91723bfa45189f0aa
Saeed Raza <sraza@aiondigital.com>
2020-10-15 07:13:12 +0500

    refactored: [APS-475] added class-transformer-validator in leave test cases

--- retail/middleware/entitlement.service/src/app/v1/leave/leave.e2e-spec.ts
+++ retail/middleware/entitlement.service/src/app/v1/leave/leave.e2e-spec.ts
@@ -1,13 +1,15 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import {transformAndValidateSync} from "class-transformer-validator";
+
 import {getChecksQuery, getDeleteMutation, getMutation, getQuery} from '@common/tests';
 import {V1Module} from '@app/v1/v1.module';
 import {LeaveInput} from '@app/v1/leave/leave.dto';
 import {KeyValInput} from '@common/inputs/key-val.input';
 import {LeaveTypeInput} from '@app/v1/leave_type/leave_type.dto';
 import {CreateUserInput} from '@app/v1/users/user.dto';
-
+import {Leave} from "@app/v1/leave/leave.model";
 
 describe('Leave Module (e2e)', () => {
   let app: INestApplication;
@@ -28,25 +30,35 @@ describe('Leave Module (e2e)', () => {
 
   it(`adds Leave`, done => {
     const input: LeaveInput = {
-      start_date:"2020-10-06",
-      end_date:"2020-10-06",
+      start_date:"1970-01-01",
+      end_date:"1970-01-01",
       remarks:"Approved",
       status:"ACTIVE",
       leave_type_id:leaveTypeId,
       user_id:userId
     };
+    const leave_validated: LeaveInput = transformAndValidateSync(
+        LeaveInput,
+        input,
+    ) as LeaveInput;
     return request(app.getHttpServer())
       .post('/graphql')
       .send({
-        query: getMutation("addLeave", input, "id"),
+        query: getMutation("addLeave", leave_validated, "id"),
       }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
       .expect(( {body} ) => {
-        const data = body.data.addLeave;
-        leaveId = data.id;
-        expect(data.id).toBeDefined();
+        const data = body?.data?.addLeave;
+        leaveId = data?.id;
+        const leave_json: string = JSON.stringify(data);
+        const leave_validated: Leave = transformAndValidateSync(
+            Leave,
+            leave_json,
+        ) as Leave;
+        expect(leave_validated).toBeDefined();
+        expect(leave_validated).toBeInstanceOf(Leave);
       })
       .expect(200)
       .end(done);
@@ -54,24 +66,35 @@ describe('Leave Module (e2e)', () => {
 
   it(`updates Leave`, done => {
     const input: LeaveInput = {
-      start_date:"2020-10-06",
-      end_date:"2020-10-06",
+      start_date:"1970-01-01",
+      end_date:"1970-01-01",
       remarks:"Not-Approved",
       status:"ACTIVE",
       leave_type_id:leaveTypeId,
       user_id:userId
     };
+    const leave_validated: LeaveInput = transformAndValidateSync(
+        LeaveInput,
+        input,
+    ) as LeaveInput;
     return request(app.getHttpServer())
     .post('/graphql')
     .send({
-      query: getMutation("updateLeave", input, "id remarks", leaveId),
+      query: getMutation("updateLeave", leave_validated, "id remarks", leaveId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.updateLeave;
-      expect(data.remarks).toBe("Not-Approved");
+      const data = body?.data?.updateLeave;
+      expect(data?.remarks).toBe("Not-Approved");
+      const leave_json: string = JSON.stringify(data);
+      const leave_validated: Leave = transformAndValidateSync(
+          Leave,
+          leave_json,
+      ) as Leave;
+      expect(leave_validated).toBeDefined();
+      expect(leave_validated).toBeInstanceOf(Leave);
     })
     .expect(200)
     .end(done);
@@ -83,12 +106,19 @@ describe('Leave Module (e2e)', () => {
     .send({
       query: getQuery("leavesList", "id remarks"),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.leavesList;
-      expect(data.length).toBeDefined();
+      const data = body?.data?.leavesList;
+      expect(data?.length).toBeDefined();
+      const leave_json: string = JSON.stringify(data);
+      const leave_validated: Leave[] = transformAndValidateSync(
+          Leave,
+          leave_json,
+      ) as Leave[];
+      expect(leave_validated).toBeDefined();
+      expect(leave_validated).toBeInstanceOf(Array);
     })
     .expect(200)
     .end(done);
@@ -100,12 +130,18 @@ describe('Leave Module (e2e)', () => {
     .send({
       query: getQuery("findLeaveById", "id remarks", leaveId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.findLeaveById;
-      expect(data.id).toBeDefined();
+      const data = body?.data?.findLeaveById;
+      const leave_json: string = JSON.stringify(data);
+      const leave_validated: Leave = transformAndValidateSync(
+          Leave,
+          leave_json,
+      ) as Leave;
+      expect(leave_validated).toBeDefined();
+      expect(leave_validated).toBeInstanceOf(Leave);
     })
     .expect(200)
     .end(done);
@@ -113,19 +149,26 @@ describe('Leave Module (e2e)', () => {
 
   it(`searches Leaves by properties`, done => {
     const checks: KeyValInput[] = [
-      {record_key: "remarks", record_value: "Approved"}
+      {record_key: "remarks", record_value: "Not-Approved"}
     ];
     return request(app.getHttpServer())
     .post('/graphql')
     .send({
       query: getChecksQuery("findLeaveBy", checks, "id remarks"),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.findLeaveBy;
-      expect(data.length).toBeDefined();
+      const data = body?.data?.findLeaveBy;
+      expect(data?.length).toBeDefined();
+      const leave_json: string = JSON.stringify(data);
+      const leave_validated: Leave[] = transformAndValidateSync(
+          Leave,
+          leave_json,
+      ) as Leave[];
+      expect(leave_validated).toBeDefined();
+      expect(leave_validated).toBeInstanceOf(Array);
     })
     .expect(200)
     .end(done);
@@ -137,11 +180,11 @@ describe('Leave Module (e2e)', () => {
     .send({
       query: getDeleteMutation("deleteLeave", leaveId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.deleteLeave;
+      const data = body?.data?.deleteLeave;
       expect(data).toBeTruthy();
     })
     .expect(200)
@@ -154,12 +197,12 @@ describe('Leave Module (e2e)', () => {
     .send({
       query: getQuery("findLeaveById", "id remarks", leaveId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const [error] = body.errors;
-      expect(error.message).toBe("Leave Not Found");
+      const [error] = body?.errors;
+      expect(error?.message).toBe("Leave Not Found");
     })
     .expect(200)
     .end(done);
@@ -167,24 +210,28 @@ describe('Leave Module (e2e)', () => {
 
   it(`should return error of Leave start date should be less than end date`, done => {
     const input: LeaveInput = {
-      start_date:"2020-10-07",
-      end_date:"2020-10-06",
+      start_date:"1970-01-02",
+      end_date:"1970-01-01",
       remarks:"Approved",
       status:"ACTIVE",
       leave_type_id:leaveTypeId,
       user_id:userId
     };
+    const leave_validated: LeaveInput = transformAndValidateSync(
+        LeaveInput,
+        input,
+    ) as LeaveInput;
     return request(app.getHttpServer())
     .post('/graphql')
     .send({
-      query: getMutation("addLeave", input, "id"),
+      query: getMutation("addLeave", leave_validated, "id"),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const [error] = body.errors;
-      expect(error.message).toBe("Leave start date should be less than end date");
+      const [error] = body?.errors;
+      expect(error?.message).toBe("Leave start date should be less than end date");
     })
     .expect(200)
     .end(done);
@@ -192,24 +239,28 @@ describe('Leave Module (e2e)', () => {
 
   it(`should return error of Leave Type Not Found`, done => {
     const input: LeaveInput = {
-      start_date:"2020-10-06",
-      end_date:"2020-10-06",
+      start_date:"1970-01-01",
+      end_date:"1970-01-01",
       remarks:"Approved",
       status:"ACTIVE",
       leave_type_id:"58B630C1-B884-43B1-AE17-E7214FDB09F7",
       user_id:userId
     };
+    const leave_validated: LeaveInput = transformAndValidateSync(
+        LeaveInput,
+        input,
+    ) as LeaveInput;
     return request(app.getHttpServer())
     .post('/graphql')
     .send({
-      query: getMutation("addLeave", input, "id"),
+      query: getMutation("addLeave", leave_validated, "id"),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const [error] = body.errors;
-      expect(error.message).toBe("Leave Type Not Found");
+      const [error] = body?.errors;
+      expect(error?.message).toBe("Leave Type Not Found");
     })
     .expect(200)
     .end(done);
@@ -217,24 +268,28 @@ describe('Leave Module (e2e)', () => {
 
   it(`should return error of User Not Found`, done => {
     const input: LeaveInput = {
-      start_date:"2020-10-06",
-      end_date:"2020-10-06",
+      start_date:"1970-01-01",
+      end_date:"1970-01-01",
       remarks:"Approved",
       status:"ACTIVE",
       leave_type_id:leaveTypeId,
       user_id:"58B630C1-B884-43B1-AE17-E7214FDB09F7"
     };
+    const leave_validated: LeaveInput = transformAndValidateSync(
+        LeaveInput,
+        input,
+    ) as LeaveInput;
     return request(app.getHttpServer())
     .post('/graphql')
     .send({
-      query: getMutation("addLeave", input, "id"),
+      query: getMutation("addLeave", leave_validated, "id"),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const [error] = body.errors;
-      expect(error.message).toBe("User Not Found");
+      const [error] = body?.errors;
+      expect(error?.message).toBe("User Not Found");
     })
     .expect(200)
     .end(done);
@@ -249,7 +304,7 @@ describe('Leave Module (e2e)', () => {
 
 async function addLeaveType(app: INestApplication): Promise<string> {
   const input: LeaveTypeInput = {
-    name: "temp-e2e-testing",
+    name: "leave_type_e2e_testing",
     status: "ACTIVE"
   };
   const res = await request(app.getHttpServer())
@@ -257,16 +312,16 @@ async function addLeaveType(app: INestApplication): Promise<string> {
   .send({
     query: getMutation("addLeaveType", input, "id"),
   }).set({
-    "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-    "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+        "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+        "x-user-id": process.env.ENV_RBX_E2E_USER_ID
   });
-  return res.body.data.addLeaveType.id;
+  return res?.body?.data?.addLeaveType?.id;
 }
 
 async function addUser(app: INestApplication): Promise<string> {
   const input2: CreateUserInput = {
-    email:"tempe2e@jest.com",
-    first_name: "tempe2e",
+    email:"leave_type_e2e_testing@jest.com",
+    first_name: "leave_type_e2e_testing",
     last_name: "jest",
     status: "ACTIVE",
     password: "something"
@@ -276,10 +331,10 @@ async function addUser(app: INestApplication): Promise<string> {
   .send({
     query: getMutation("addUser", input2, "id"),
   }).set({
-    "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-    "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+        "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+        "x-user-id": process.env.ENV_RBX_E2E_USER_ID
   });
-  return res.body.data.addUser.id;
+  return res?.body?.data?.addUser?.id;
 }
 
 async function deleteUser(app: INestApplication, userId: string): Promise<void> {
@@ -288,8 +343,8 @@ async function deleteUser(app: INestApplication, userId: string): Promise<void> 
   .send({
     query: getDeleteMutation("deleteUser", userId),
   }).set({
-    "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-    "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+        "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+        "x-user-id": process.env.ENV_RBX_E2E_USER_ID
   });
 }
 
@@ -299,7 +354,7 @@ async function deleteLeaveType(app: INestApplication, leaveTypeId: string): Prom
   .send({
     query: getDeleteMutation("deleteLeaveType", leaveTypeId),
   }).set({
-    "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-    "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+        "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+        "x-user-id": process.env.ENV_RBX_E2E_USER_ID
   });
 }
--- retail/middleware/entitlement.service/src/app/v1/leave/leave.model.ts
+++ retail/middleware/entitlement.service/src/app/v1/leave/leave.model.ts
@@ -1,54 +1,85 @@
 import { Field, ID, ObjectType } from "@nestjs/graphql";
 import {PaginationModel} from '@common/models';
 import {Type} from 'class-transformer';
+import {IsOptional, IsString, IsUUID} from "class-validator";
 
 @ObjectType()
 export class Leave {
   @Field(() => ID,{ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   id: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   user_id: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   leave_type_id: string;
 
   @Field({ nullable: true })
   @Type(() => Date)
+  @IsOptional()
   start_date: Date;
 
   @Field({ nullable: true })
   @Type(() => Date)
+  @IsOptional()
   end_date: Date;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   remarks: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   status: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   tenant_id: string;
 
   @Field({ nullable: true })
   @Type(() => Date)
+  @IsOptional()
   created_on: Date;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   created_by: string;
 
   @Field({ nullable: true })
   @Type(() => Date)
+  @IsOptional()
   updated_on: Date;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   updated_by: string;
 
   @Field({ nullable: true })
   @Type(() => Date)
+  @IsOptional()
   deleted_on: Date;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   deleted_by: string;
 }
 
