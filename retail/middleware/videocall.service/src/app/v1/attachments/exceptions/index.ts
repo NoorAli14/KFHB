3968aac45b00fdcc6ec6153bfdbfae5823d0f035
Sajid Ali <sajid@aiondigital.com>
2020-10-15 18:25:21 +0500

    fea: [APS-646], added custom exceptions and error code

--- retail/middleware/videocall.service/src/app/v1/attachments/exceptions/index.ts
+++ retail/middleware/videocall.service/src/app/v1/attachments/exceptions/index.ts
@@ -1,1 +1,3 @@
 export * from './attachment-not-found.exception';
+export * from './invalid-base64.exception';
+export * from './not-created.exception';
---
+++ retail/middleware/videocall.service/src/app/v1/attachments/exceptions/invalid-base64.exception.ts
@@ -0,0 +1,12 @@
+import { HttpStatus } from '@nestjs/common';
+import { BaseException } from '@common/exceptions';
+export class InvalidBase64Exception extends BaseException {
+  constructor(screenshot_id?: string | any) {
+    super(HttpStatus.BAD_REQUEST, {
+      message: `Invalid base64 string`,
+      errorCode: 'VC-1067',
+      name: 'INVALID_BASE64_STRING',
+      developerMessage: `Invalid base64 string, for attachment having Id: [${screenshot_id}]`,
+    });
+  }
+}
