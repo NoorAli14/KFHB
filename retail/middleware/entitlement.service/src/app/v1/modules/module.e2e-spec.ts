af280533e90464cfd8e8baff84b9709658f1018f
Saeed Raza <sraza@aiondigital.com>
2020-10-15 07:14:09 +0500

    refactored: [APS-475] added class-transformer-validator in modules test cases

--- retail/middleware/entitlement.service/src/app/v1/modules/module.e2e-spec.ts
+++ retail/middleware/entitlement.service/src/app/v1/modules/module.e2e-spec.ts
@@ -1,10 +1,13 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import {transformAndValidateSync} from "class-transformer-validator";
+
 import {getChecksQuery, getDeleteMutation, getMutation, getQuery} from '@common/tests';
 import {V1Module} from '@app/v1/v1.module';
 import {ModuleInput} from '@app/v1/modules/module.dto';
 import {KeyValInput} from '@common/inputs/key-val.input';
+import {Module} from "@app/v1/modules/module.model";
 
 
 describe('Module Module (e2e)', () => {
@@ -22,21 +25,31 @@ describe('Module Module (e2e)', () => {
 
   it(`adds Module`, done => {
     const input: ModuleInput = {
-      name: "E2E-testing",
+      name: "module_e2e_testing",
       status: "ACTIVE"
     };
+    const input_validated: ModuleInput = transformAndValidateSync(
+        ModuleInput,
+        input,
+    ) as ModuleInput;
     return request(app.getHttpServer())
       .post('/graphql')
       .send({
-        query: getMutation("addModule", input, "id"),
+        query: getMutation("addModule", input_validated, "id"),
       }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
       .expect(( {body} ) => {
-        const data = body.data.addModule;
-        moduleId = data.id;
-        expect(data.id).toBeDefined();
+        const data = body?.data?.addModule;
+        moduleId = data?.id;
+        const module_json: string = JSON.stringify(data);
+        const module_validated: Module = transformAndValidateSync(
+            Module,
+            module_json,
+        ) as Module;
+        expect(module_validated).toBeDefined();
+        expect(module_validated).toBeInstanceOf(Module);
       })
       .expect(200)
       .end(done);
@@ -44,20 +57,31 @@ describe('Module Module (e2e)', () => {
 
   it(`updates Module`, done => {
     const input: ModuleInput = {
-      name: "E2E-updated",
+      name: "module_e2e_testing_updated",
       status: "ACTIVE"
     };
+    const input_validated: ModuleInput = transformAndValidateSync(
+        ModuleInput,
+        input,
+    ) as ModuleInput;
     return request(app.getHttpServer())
     .post('/graphql')
     .send({
-      query: getMutation("updateModule", input, "id name", moduleId),
+      query: getMutation("updateModule", input_validated, "id name", moduleId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.updateModule;
-      expect(data.name).toBe("E2E-updated");
+      const data = body?.data?.updateModule;
+      expect(data?.name).toBe("module_e2e_testing_updated");
+      const module_json: string = JSON.stringify(data);
+      const module_validated: Module = transformAndValidateSync(
+          Module,
+          module_json,
+      ) as Module;
+      expect(module_validated).toBeDefined();
+      expect(module_validated).toBeInstanceOf(Module);
     })
     .expect(200)
     .end(done);
@@ -69,12 +93,19 @@ describe('Module Module (e2e)', () => {
     .send({
       query: getQuery("modulesList", "id name"),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.modulesList;
-      expect(data.length).toBeDefined();
+      const data = body?.data?.modulesList;
+      expect(data?.length).toBeDefined();
+      const module_json: string = JSON.stringify(data);
+      const module_validated: Module[] = transformAndValidateSync(
+          Module,
+          module_json,
+      ) as Module[];
+      expect(module_validated).toBeDefined();
+      expect(module_validated).toBeInstanceOf(Array);
     })
     .expect(200)
     .end(done);
@@ -86,12 +117,18 @@ describe('Module Module (e2e)', () => {
     .send({
       query: getQuery("findModuleById", "id name", moduleId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.findModuleById;
-      expect(data.id).toBeDefined();
+      const data = body?.data?.findModuleById;
+      const module_json: string = JSON.stringify(data);
+      const module_validated: Module = transformAndValidateSync(
+          Module,
+          module_json,
+      ) as Module;
+      expect(module_validated).toBeDefined();
+      expect(module_validated).toBeInstanceOf(Module);
     })
     .expect(200)
     .end(done);
@@ -99,19 +136,26 @@ describe('Module Module (e2e)', () => {
 
   it(`searches Modules by properties`, done => {
     const checks: KeyValInput[] = [
-      {record_key: "name", record_value: "E2E-testing"}
+      {record_key: "name", record_value: "module_e2e_testing_updated"}
     ];
     return request(app.getHttpServer())
     .post('/graphql')
     .send({
       query: getChecksQuery("findModuleBy", checks, "id name"),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.findModuleBy;
-      expect(data.length).toBeDefined();
+      const data = body?.data?.findModuleBy;
+      expect(data?.length).toBeDefined();
+      const module_json: string = JSON.stringify(data);
+      const module_validated: Module[] = transformAndValidateSync(
+          Module,
+          module_json,
+      ) as Module[];
+      expect(module_validated).toBeDefined();
+      expect(module_validated).toBeInstanceOf(Array);
     })
     .expect(200)
     .end(done);
@@ -123,11 +167,11 @@ describe('Module Module (e2e)', () => {
     .send({
       query: getDeleteMutation("deleteModule", moduleId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const data = body.data.deleteModule;
+      const data = body?.data?.deleteModule;
       expect(data).toBeTruthy();
     })
     .expect(200)
@@ -140,12 +184,12 @@ describe('Module Module (e2e)', () => {
     .send({
       query: getQuery("findModuleById", "id name", moduleId),
     }).set({
-      "x-tenant-id": "58B630C1-B884-43B1-AE17-E7214FDB09F7",
-      "x-user-id": "289CB901-C8CB-444A-A0F0-2452019D7E0D"
+          "x-tenant-id": process.env.ENV_RBX_E2E_TENANT_ID,
+          "x-user-id": process.env.ENV_RBX_E2E_USER_ID
     })
     .expect(( {body} ) => {
-      const [error] = body.errors;
-      expect(error.message).toBe("Module Not Found");
+      const [error] = body?.errors;
+      expect(error?.message).toBe("Module Not Found");
     })
     .expect(200)
     .end(done);
--- retail/middleware/entitlement.service/src/app/v1/modules/module.model.ts
+++ retail/middleware/entitlement.service/src/app/v1/modules/module.model.ts
@@ -1,34 +1,54 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Permission } from '@app/v1/permissions/permission.model';
 import { PaginationModel } from '@common/models';
+import {IsOptional, IsString, IsUUID} from "class-validator";
 
 @ObjectType()
 export class Module {
   @Field(() => ID, { nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   id?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   name?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   slug?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   parent_id?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   status?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   created_on?: string;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  @IsUUID()
   created_by?: string;
 
   @Field(() => [Module], { nullable: true })
+  @IsOptional()
   sub_modules?: Module[];
 
   @Field(() => [Permission], { nullable: true })
+  @IsOptional()
   permissions?: Permission[];
 }
 
