08411b0907ada46506008f5282f20980515a1141
Muhammad Saeed Raza <sraza@aiondigital.com>
2020-10-20 11:54:01 +0500

    feat: [APS-667] changed created_on param to a JSON object

--- retail/middleware/identity.service/src/app/v1/customers/decorators/query-params-customer.decorator.ts
+++ retail/middleware/identity.service/src/app/v1/customers/decorators/query-params-customer.decorator.ts
@@ -6,19 +6,24 @@ export const QueryParamsCustomer = createParamDecorator(
   async (data: any, context: ExecutionContext & { [key: string]: any }) => {
     const ctx: any = GqlExecutionContext.create(context);
     const { req } = ctx.getContext();
-    return {
-        page: req.query[PAGINATION_PARAMS.PAGE],
-        limit: req.query[PAGINATION_PARAMS.LIMIT],
-        national_id_no: req.query[PAGINATION_PARAMS.NATIONAL_ID_NO],
-        gender: req.query[PAGINATION_PARAMS.GENDER],
-        nationality: req.query[PAGINATION_PARAMS.NATIONALITY],
-        first_name: req.query[PAGINATION_PARAMS.FIRST_NAME],
-        last_name: req.query[PAGINATION_PARAMS.LAST_NAME],
-        status: req.query[PAGINATION_PARAMS.STATUS],
-        contact_no: req.query[PAGINATION_PARAMS.CONTACT_NO],
-        email: req.query[PAGINATION_PARAMS.EMAIL],
-        created_on_start: req.query[PAGINATION_PARAMS.CREATED_ON_START],
-        created_on_end: req.query[PAGINATION_PARAMS.CREATED_ON_END],
-    };
+    let created_on = {};
+    try {
+        created_on = JSON.parse(req.query["created_on"]);
+    } finally {
+        return {
+            page: req.query[PAGINATION_PARAMS.PAGE],
+            limit: req.query[PAGINATION_PARAMS.LIMIT],
+            national_id_no: req.query[PAGINATION_PARAMS.NATIONAL_ID_NO],
+            gender: req.query[PAGINATION_PARAMS.GENDER],
+            nationality: req.query[PAGINATION_PARAMS.NATIONALITY],
+            first_name: req.query[PAGINATION_PARAMS.FIRST_NAME],
+            last_name: req.query[PAGINATION_PARAMS.LAST_NAME],
+            status: req.query[PAGINATION_PARAMS.STATUS],
+            contact_no: req.query[PAGINATION_PARAMS.CONTACT_NO],
+            email: req.query[PAGINATION_PARAMS.EMAIL],
+            created_on_start: created_on["start"],
+            created_on_end: created_on["end"],
+        };
+    }
   },
 );
