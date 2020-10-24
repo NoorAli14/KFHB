59bfc571b6d872bac7cab1d56db5a27727f13b66
Faizan AH <faizan@aiondigital.com>
2020-10-24 16:11:03 +0500

    fix: [APS-667] fix sorting issue

--- retail/middleware/onboarding.business.service/src/app/v1/customers/customers.controller.ts
+++ retail/middleware/onboarding.business.service/src/app/v1/customers/customers.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, UseGuards, Get, Query } from '@nestjs/common';
 import { ApiTags, ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { CustomersService } from './customers.service';
-import { AuthGuard, PaginationDTO, SortByDTO } from '@common/index';
+import { AuthGuard, PaginationDTO } from '@common/index';
+import { SortByDTO } from '@common/dtos/sort_by.dto';
+
 import { CustomerPaginationList } from './customer.entity';
 import { CustomerFilterDTO } from './dtos/customer-filter.dto';
 
@@ -21,7 +23,14 @@ export class CustomerController {
     })
     @ApiOkResponse({ type: CustomerPaginationList, description: 'List of all customers.' })
     // @Permissions('view:users')
-    async list(@Query() pagination: PaginationDTO, @Query() filters: CustomerFilterDTO, @Query() sort_by: [SortByDTO]): Promise<CustomerPaginationList> {
+    async list(@Query() pagination: PaginationDTO, @Query() filters: CustomerFilterDTO, @Query() order: SortByDTO): Promise<CustomerPaginationList> {
+        let sort_by = [];
+        if (order) {
+            sort_by = [{
+                field: order?.sort_by || 'created_on',
+                direction: order?.sort_order || 'desc'
+            }]
+        }
         return this.customerService.list({ pagination, filters, sort_by });
     }
 }
