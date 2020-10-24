bb9725c545846ba4cc84b440872e5c66e9a1c11c
Muhammad Ali Shah <mshah@aiondigital.com>
2020-10-24 19:22:12 +0400

    fix: [APS-306] Fixed type paths errors and added logic to generate output in separate schema files

--- docs/env-mgmt/database/index.ts
+++ docs/env-mgmt/database/index.ts
@@ -1,87 +1,69 @@
 import * as fs from 'fs';
-import * as Knex from 'knex';
-const moduleAlias = require('module-alias');
+import { resolve } from 'path';
+
+
+const RBX_SERVICE_NAME = process.env.RBX_SERVICE_NAME;
+const FILENAME_SCHEMA_OUTPUT = `schema-${RBX_SERVICE_NAME}.sql`;
+const DIR_MIDDLEWARE = resolve(`../../../retail/middleware/`);
+const DIR_MIGRATION = `${DIR_MIDDLEWARE}/${RBX_SERVICE_NAME}/src/core/database/migrations/`;
+const DIR_OUTPUT = "generated-schema";
+const PATH_SCHEMA_OUTPUT = `./${DIR_OUTPUT}/${FILENAME_SCHEMA_OUTPUT}`;
+
 
 (async () => {
     try {
-        const middlewarePATH = `../../../retail/middleware/`;
-        // const dbConfig: DBConfigurationService = new DBConfigurationService();
+        // db driver to generate the queries
+        const knex = require('knex')({ client: 'mssql' });
 
-        const connection = require('knex')({ client: 'mssql' });
+        validateStartupParams();
 
-        // list of services
-        const services: string[] = [
-            'compliance.service',
-            'entitlement.service',
-            'identity.service',
-            'notifications',
-            'reference.service',
-            'videocall.service'
-        ];
+        // get all migration files for the specified service        
+        const migrationFiles = fs.readdirSync(`${DIR_MIGRATION}`);
 
-        //Get list of all available folders in retail middleware
-        const middlewareDir = getDirectories(middlewarePATH);
+        // process each file and generate the output in a string
+        let schema = "";
+        for (let file of migrationFiles) {
+            const filename = file.normalize();
+            console.log(`Processing [${filename}]`);
 
-        // Find if any define service is missing in middleware folder
-        const missingService = services.filter(service =>
-            !middlewareDir.includes(service),
-        );
-
-        // Check if any required service is missing
-        if (missingService?.length > 0) throw new Error(`Middleware services are missing: [${missingService}]`);
-
-        const migrations = services.map(service => {
-            return {
-                service: {
-                    name: service,
-                    migrationDir: `${middlewarePATH}/${service}/src/core/database/migrations/`
-                },
-                files: fs.readdirSync(`${middlewarePATH}/${service}/src/core/database/migrations/`)
-            }
-        });
-
-        // Loop through all the existing migrations files
-        let schema = '';
-        for (let migration of migrations) {
-            const serviceLoc = `${middlewarePATH}${migration.service.name}`;
-            const tsConfig = require(`${serviceLoc}/tsconfig.json`);
-            const tsConfigPaths = require("tsconfig-paths");
-            let paths = {};
-            const middlewareDir1 = getDirectories(`${serviceLoc}/`);
-            console.log(middlewareDir1);
-
-            for (let path in tsConfig.compilerOptions.paths) {
-                const _path = tsConfig.compilerOptions.paths[path][0];
-                paths[path] = [_path.replace('./', `${serviceLoc}/`)];
-                console.log(`${path}  ->  ${_path.replace('./', `${__dirname}${serviceLoc}/`)}`)
-                moduleAlias.addAlias(path, _path.replace('./', `${serviceLoc}/`));
-
-            }
-            console.log(JSON.stringify(paths, null, 2));
-            // const cleanup = tsConfigPaths.register({
-            //     baseUrl: serviceLoc,
-            //     paths
-            // });
-            // When path registration is no longer needed
-            for (let filename of migration.files) {
-                console.log(`---> Start exporting ${filename}\n`);
-                // Parse date from migration filename
-                const date = filename.split('_')[0];
-                const comment = `/* [${migration.service.name}] [${date}] - ${filename} */`;
-
-                // Read migration file query into string
-                schema = schema + comment + await getSQL(connection, `${migration.service.migrationDir}/${filename}`) + ';\r\n\n\n';
-            }
-
-
+            // Parse date from migration filename
+            const date = filename.split('_')[0];
+            const comment = `/* [${RBX_SERVICE_NAME}] [${date}] - ${filename} */\r\n`;
+            schema += comment + await getSQL(knex, `${DIR_MIGRATION}/${filename}`) + ';\r\n\n\n';
         }
-        await fs.promises.truncate('schema.sql', 0);
-        await fs.promises.appendFile('schema.sql', `${schema};\r\n\n\n`);
+
+        // write the generate schema to the output file
+        await fs.promises.writeFile(PATH_SCHEMA_OUTPUT, `${schema};\r\n\n\n`);
+
     } catch (err) {
-        console.log(err);
+        console.error(err);
         process.exit(1);
     }
 })();
+
+
+
+// Validate and log parameters for startup
+function validateStartupParams() {
+    console.log ('---------------')
+    console.log(`Running for service:         [${RBX_SERVICE_NAME}]\r`);
+    console.log(`Using middleware path:       [${DIR_MIDDLEWARE}]`);
+    console.log(`Reading migration files from [${DIR_MIGRATION}]`);
+    console.log(`Writing to output filename:  [${PATH_SCHEMA_OUTPUT}]`);
+    console.log ('---------------')
+
+    if (RBX_SERVICE_NAME == undefined) {
+        console.error('Undefined service name. Please define the RBX_SERVICE_NAME env variable.');
+        process.exit(1);
+    }
+
+    if (fs.existsSync(PATH_SCHEMA_OUTPUT)) {
+        console.error(`Output file [${PATH_SCHEMA_OUTPUT}] already exists; cannot continue!`);
+        process.exit(1);
+    }
+}
+
+
 
 /**
  * This method is used to generate a raw sql query
