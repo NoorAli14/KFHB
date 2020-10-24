c0f8690960c0f0e6dcfafae494da48e364b303e8
Faizan AH <faizan@aiondigital.com>
2020-10-24 19:09:42 +0500

    fix: [APS-726] update complaince template last steps

--- retail/middleware/onboarding.business.service/src/app/v1/compliances/compliances.controller.ts
+++ retail/middleware/onboarding.business.service/src/app/v1/compliances/compliances.controller.ts
@@ -4,8 +4,6 @@ import {
   Post,
   UseGuards,
   NotFoundException,
-  ParseUUIDPipe,
-  Param,
   HttpCode,
   HttpStatus,
   Body,
@@ -27,7 +25,6 @@ import { ComplianceDto } from './compliance.dto';
 import { ITemplateResponse } from './compliance.interface';
 import { Customer } from '../customers/customer.entity';
 import { CustomersService } from '../customers/customers.service';
-import { User } from '../users/user.entity';
 @ApiTags('Compliance Module')
 @Controller('compliance')
 @ApiBearerAuth()
@@ -58,15 +55,11 @@ export class CompliancesController {
     type: Error,
     description: 'Template Not Found.',
   })
-  async crs(@CurrentUser() currentUser: User): Promise<Template> {
+  async crs(): Promise<Template> {
     const compliance: Template = await this.complianceService.findOneByName(
       this.__template.CRS,
     );
     if (!compliance) throw new NotFoundException(`Template not found.`);
-    await this.customerService.updateLastStep(
-      currentUser.id,
-      CUSTOMER_LAST_STEPS.RBX_ONB_STEP_CRS_SUBMITTED,
-    );
     return compliance;
   }
 
@@ -84,15 +77,11 @@ export class CompliancesController {
     type: Error,
     description: 'Template Not Found.',
   })
-  async kyc(@CurrentUser() currentUser: User): Promise<Template> {
+  async kyc(): Promise<Template> {
     const compliance: Template = await this.complianceService.findOneByName(
       this.__template.KYC,
     );
     if (!compliance) throw new NotFoundException(`Template not found.`);
-    await this.customerService.updateLastStep(
-      currentUser.id,
-      CUSTOMER_LAST_STEPS.RBX_ONB_STEP_KYC_SUBMITTED,
-    );
     return compliance;
   }
 
@@ -110,47 +99,97 @@ export class CompliancesController {
     type: Error,
     description: 'Template Not Found.',
   })
-  async fatca(@CurrentUser() currentUser: User): Promise<Template> {
+  async fatca(): Promise<Template> {
     const compliance: Template = await this.complianceService.findOneByName(
       this.__template.FATCA,
     );
     if (!compliance) throw new NotFoundException(`Template not found.`);
-    await this.customerService.updateLastStep(
-      currentUser.id,
-      CUSTOMER_LAST_STEPS.RBX_ONB_STEP_FATCA_SUBMITTED,
-    );
     return compliance;
   }
 
-  @Post(':template_id')
+  @Post('fatca')
   @ApiBody({
     description: 'Sets the template properties.',
     type: ComplianceDto,
   })
   @ApiOperation({
-    summary: 'Submit compliance response',
+    summary: 'Submit FATCA response',
     description: 'A successful request returns the HTTP 200 OK status code.',
   })
   @ApiOkResponse({
     type: Template,
-    description: 'Template response has been successfully submitted.',
+    description: 'FATCA response has been successfully submitted.',
   })
   @ApiNotFoundResponse({
     type: Error,
     description: 'Template Not Found.',
   })
   @HttpCode(HttpStatus.OK)
-  async submit(
-    @Param('template_id', ParseUUIDPipe) template_id: string,
-    @CurrentUser() customer: Customer,
-    @Body() input: ComplianceDto,
-  ): Promise<Template> {
+  async submitFatca(@CurrentUser() currentUser: Customer, @Body() input: ComplianceDto): Promise<Template> {
+    return this.submitResponse(this.__template.FATCA, currentUser.id, CUSTOMER_LAST_STEPS.RBX_ONB_STEP_FATCA_SUBMITTED, input);
+  }
+
+  @Post('crs')
+  @ApiBody({
+    description: 'Sets the template properties.',
+    type: ComplianceDto,
+  })
+  @ApiOperation({
+    summary: 'Submit CRS response',
+    description: 'A successful request returns the HTTP 200 OK status code.',
+  })
+  @ApiOkResponse({
+    type: Template,
+    description: 'CRS response has been successfully submitted.',
+  })
+  @ApiNotFoundResponse({
+    type: Error,
+    description: 'Template Not Found.',
+  })
+  @HttpCode(HttpStatus.OK)
+  async submitCRS(@CurrentUser() currentUser: Customer, @Body() input: ComplianceDto): Promise<Template> {
+    return this.submitResponse(this.__template.CRS, currentUser.id, CUSTOMER_LAST_STEPS.RBX_ONB_STEP_CRS_SUBMITTED, input);
+  }
+
+  @Post('kyc')
+  @ApiBody({
+    description: 'Sets the template properties.',
+    type: ComplianceDto,
+  })
+  @ApiOperation({
+    summary: 'Submit KYC response',
+    description: 'A successful request returns the HTTP 200 OK status code.',
+  })
+  @ApiOkResponse({
+    type: Template,
+    description: 'KYC response has been successfully submitted.',
+  })
+  @ApiNotFoundResponse({
+    type: Error,
+    description: 'Template Not Found.',
+  })
+  @HttpCode(HttpStatus.OK)
+  async submitKYC(@CurrentUser() currentUser: Customer, @Body() input: ComplianceDto): Promise<Template> {
+    return this.submitResponse(this.__template.KYC, currentUser.id, CUSTOMER_LAST_STEPS.RBX_ONB_STEP_KYC_SUBMITTED, input);
+  }
+  async submitResponse(name: string, user_id: string, last_step: string, input: ComplianceDto): Promise<Template> {
+    const compliance: Template = await this.complianceService.findOneByName(
+      name,
+    );
+    if (!compliance) throw new NotFoundException(`Template not found.`);
     const params: ITemplateResponse = {
-      template_id: template_id,
-      user_id: customer.id,
+      template_id: compliance.id,
+      user_id: user_id,
       results: input.results,
       remarks: input.remarks,
     };
-    return this.complianceService.submitResponse(params);
+    const result = await this.complianceService.submitResponse(params);
+    if (result) {
+      await this.customerService.updateLastStep(
+        user_id,
+        last_step,
+      );
+    }
+    return result;
   }
 }
