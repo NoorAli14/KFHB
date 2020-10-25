1d15d31b16a941af25ebb2bb7a35ca9fea98a127
Sajid Ali <sajid@aiondigital.com>
2020-10-25 14:07:44 +0500

    fea: [APS-747], added end point to fetch attachment steam

--- retail/middleware/onboarding.business.service/src/app/v1/attachments/attachments.controller.ts
+++ retail/middleware/onboarding.business.service/src/app/v1/attachments/attachments.controller.ts
@@ -1,18 +1,19 @@
 import {
-    Controller,
-    UseGuards,
-    ParseUUIDPipe,
-    Param,
-    Get,
-    Post, Body
+  Controller,
+  UseGuards,
+  ParseUUIDPipe,
+  Param,
+  Get,
+  Post,
+  Body,
 } from '@nestjs/common';
 import {
-    ApiTags,
-    ApiOperation,
-    ApiBearerAuth,
-    ApiOkResponse,
-    ApiCreatedResponse,
-    ApiBadRequestResponse,
+  ApiTags,
+  ApiOperation,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
 } from '@nestjs/swagger';
 import { Attachment } from './attachment.entity';
 import { CreateAttachmentDTO } from './attachment.dto';
@@ -25,47 +26,65 @@ import { AttachmentUploadingInput } from './attachment.interface';
 // @UseGuards(AuthGuard)
 @Controller()
 export class AttachmentsController {
-    constructor(private readonly attachmentService: AttachmentsService) { }
+  constructor(private readonly attachmentService: AttachmentsService) {}
 
-    @Post('customers/:customer_id/attachments')
-    @ApiOperation({
-        summary: 'Upload a customer attachment',
-        description:
-            'A successful request returns the HTTP 201 CREATED status code and a JSON response body that shows attachment information.',
-    })
-    @ApiCreatedResponse({
-        type: Attachment,
-        description: 'Attachment has been successfully uploaded.',
-    })
-    @ApiBadRequestResponse({
-        type: Error,
-        description: 'Input Validation failed.',
-    })
-    async create(
-        @Body() input: CreateAttachmentDTO,
-        @Param('customer_id', ParseUUIDPipe) customer_id: string,
-    ): Promise<Attachment> {
-        const params: AttachmentUploadingInput = {
-            file_content: input.file_content,
-            attachment_id: input.attachment_type,
-            customer_id
-        };
-        return this.attachmentService.upload(params);
-    }
+  @Post('customers/:customer_id/attachments')
+  @ApiOperation({
+    summary: 'Upload a customer attachment',
+    description:
+      'A successful request returns the HTTP 201 CREATED status code and a JSON response body that shows attachment information.',
+  })
+  @ApiCreatedResponse({
+    type: Attachment,
+    description: 'Attachment has been successfully uploaded.',
+  })
+  @ApiBadRequestResponse({
+    type: Error,
+    description: 'Input Validation failed.',
+  })
+  async create(
+    @Body() input: CreateAttachmentDTO,
+    @Param('customer_id', ParseUUIDPipe) customer_id: string,
+  ): Promise<Attachment> {
+    const params: AttachmentUploadingInput = {
+      file_content: input.file_content,
+      attachment_id: input.attachment_type,
+      customer_id,
+    };
+    return this.attachmentService.upload(params);
+  }
 
-    @Get('customers/:customer_id/attachments')
-    @ApiOperation({
-        summary: 'List of all the attachment by customer ID',
-        description:
-            'A successful request returns the HTTP 200 OK status code and a JSON response body that shows a attachments information.',
-    })
-    @ApiOkResponse({
-        type: [Attachment],
-        description: 'List of all the attachment by customer ID',
-    })
-    async list(
-        @Param('customer_id', ParseUUIDPipe) customer_id: string,
-    ): Promise<Attachment[]> {
-        return this.attachmentService.listByCustomerID(customer_id);
-    }
+  @Get('customers/:customer_id/attachments')
+  @ApiOperation({
+    summary: 'List of all the attachment by customer ID',
+    description:
+      'A successful request returns the HTTP 200 OK status code and a JSON response body that shows a attachments information.',
+  })
+  @ApiOkResponse({
+    type: [Attachment],
+    description: 'List of all the attachment by customer ID',
+  })
+  async list(
+    @Param('customer_id', ParseUUIDPipe) customer_id: string,
+  ): Promise<Attachment[]> {
+    return this.attachmentService.listByCustomerID(customer_id);
+  }
+
+  @Get('customers/:customer_id/attachments/:id')
+  @ApiOperation({
+    summary: 'Fetch the attachment for customer',
+    description:
+      'A successful request returns the HTTP 200 OK status code and a JSON response body that shows a attachments information.',
+  })
+  @ApiOkResponse({
+    type: [Attachment],
+    description: 'Fetch the attachment for the customer',
+  })
+  async find(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('customer_id', ParseUUIDPipe) customer_id: string,
+  ): Promise<Attachment> {
+    const response = await this.attachmentService.find(id, customer_id);
+    return response;
+  }
 }
--- retail/middleware/videocall.service/src/app/v1/appointments/exceptions/appointment-not-found.exception.ts
+++ retail/middleware/videocall.service/src/app/v1/appointments/exceptions/appointment-not-found.exception.ts
@@ -2,7 +2,6 @@ import { HttpStatus } from '@nestjs/common';
 import { BaseException } from '@common/exceptions';
 export class AppointmentNotFoundException extends BaseException {
   constructor(from_time?: string | any, to_time?: string | any) {
-    console.log(`Id -0-0-0-0-0 [${from_time}]`);
     super(HttpStatus.NOT_FOUND, {
       message: `Appointment not found`,
       errorCode: 'VC-1063',
