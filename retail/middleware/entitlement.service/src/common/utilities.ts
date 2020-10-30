13d899209b59da0eebb084023558c417b696b45a
Muhammad Zaid <mzaid@aiondigital.com>
2020-10-30 23:32:53 +0500

    feat: [APS-306] implemented sql seed generation script in entitlement service

--- retail/middleware/entitlement.service/src/common/utilities.ts
+++ retail/middleware/entitlement.service/src/common/utilities.ts
@@ -142,6 +142,14 @@ export const getCurrentTimeStamp = () => {
   return moment().format();
 }
 
+export const uuidV4 = (): string => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
+
 /**
  * DATABASE UUID GENERATION
  * @param knex
