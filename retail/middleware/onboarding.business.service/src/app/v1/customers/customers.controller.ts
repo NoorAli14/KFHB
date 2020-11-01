83ad4ca6acc28a343ab2d620fb94f39b1ce3a55c
Faizan AH <faizan@aiondigital.com>
2020-11-02 00:48:12 +0500

    feat: [APS-738] added class-validator customer filter class

--- retail/middleware/onboarding.business.service/src/app/v1/customers/customers.controller.ts
+++ retail/middleware/onboarding.business.service/src/app/v1/customers/customers.controller.ts
@@ -12,52 +12,52 @@ import { CustomerFilterDTO } from './dtos/customer-filter.dto';
 @ApiBearerAuth()
 @UseGuards(AuthGuard, PermissionsGuard)
 export class CustomerController {
-  constructor(private readonly customerService: CustomersService) {}
+    constructor(private readonly customerService: CustomersService) { }
 
-  @Get('/')
-  @ApiOperation({
-    description:
-      'A successful request returns the HTTP 200 OK status code and a JSON response body that shows list of customers information.',
-    summary: 'List of all customers.',
-  })
-  @ApiOkResponse({
-    type: CustomerPaginationList,
-    description: 'List of all customers.',
-  })
-  @Permissions('view:customers')
-  async list(
-    @Query() pagination: PaginationDTO,
-    @Query() filters: CustomerFilterDTO,
-    @Query() order: SortByDTO,
-  ): Promise<CustomerPaginationList> {
-    let sort_by = [];
-    if (order) {
-      sort_by = [
-        {
-          field: order?.sort_by || 'created_on',
-          direction: order?.sort_order || 'desc',
-        },
-      ];
+    @Get('/')
+    @ApiOperation({
+        description:
+            'A successful request returns the HTTP 200 OK status code and a JSON response body that shows list of customers information.',
+        summary: 'List of all customers.',
+    })
+    @ApiOkResponse({
+        type: CustomerPaginationList,
+        description: 'List of all customers.',
+    })
+    @Permissions('view:customers')
+    async list(
+        @Query() pagination: PaginationDTO,
+        @Query() filters: CustomerFilterDTO,
+        @Query() order: SortByDTO,
+    ): Promise<CustomerPaginationList> {
+        let sort_by = [];
+        if (order) {
+            sort_by = [
+                {
+                    field: order?.sort_by || 'created_on',
+                    direction: order?.sort_order || 'desc',
+                },
+            ];
+        }
+        return this.customerService.list({ pagination, filters, sort_by });
     }
-    return this.customerService.list({ pagination, filters, sort_by });
-  }
 
-  @Get(':id')
-  @ApiOperation({
-    summary: 'Find a customer by ID',
-    description:
-      'A successful request returns the HTTP 200 OK status code and a JSON response body that shows customer information.',
-  })
-  @ApiOkResponse({
-    type: Customer,
-    description: 'Customer has been successfully retrieved.',
-  })
-  @ApiNotFoundResponse({
-    type: Error,
-    description: 'Customer Not Found.',
-  })
-  @Permissions('attend:video')
-  async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<Customer> {
-    return this.customerService.find360(id);
-  }
+    @Get(':id')
+    @ApiOperation({
+        summary: 'Find a customer by ID',
+        description:
+            'A successful request returns the HTTP 200 OK status code and a JSON response body that shows customer information.',
+    })
+    @ApiOkResponse({
+        type: Customer,
+        description: 'Customer has been successfully retrieved.',
+    })
+    @ApiNotFoundResponse({
+        type: Error,
+        description: 'Customer Not Found.',
+    })
+    @Permissions('attend:video')
+    async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<Customer> {
+        return this.customerService.find360(id);
+    }
 }
