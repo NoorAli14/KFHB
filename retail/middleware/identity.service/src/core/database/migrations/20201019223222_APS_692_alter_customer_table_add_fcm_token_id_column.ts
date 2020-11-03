51a185a670bfccfd16a75e412ea700b375b4fee0
Muhammad Zaid <mzaid@aiondigital.com>
2020-11-03 12:03:40 +0500

    feat: [APS-306] updated migration tables for sql script generation

--- retail/middleware/identity.service/src/core/database/migrations/20201019223222_APS_692_alter_customer_table_add_fcm_token_id_column.ts
+++ retail/middleware/identity.service/src/core/database/migrations/20201019223222_APS_692_alter_customer_table_add_fcm_token_id_column.ts
@@ -1,14 +1,14 @@
 import * as Knex from "knex";
 import {TABLE} from "@common/constants";
 
-export async function up(knex: Knex): Promise<any> {
+export function up(knex: Knex): any {
     return knex.schema.table(TABLE.CUSTOMER, table =>  {
         table.string('fcm_token_id');
         table.index(['fcm_token_id'], `${TABLE.CUSTOMER}_FCM_TOKEN_ID_INDEX`);
     });
 }
 
-export async function down(knex: Knex): Promise<any> {
+export function down(knex: Knex): any {
     return knex.schema.table(TABLE.CUSTOMER, table =>  {
         table.dropIndex(['fcm_token_id'], `${TABLE.CUSTOMER}_FCM_TOKEN_ID_INDEX`);
         table.dropColumn('fcm_token_id');
