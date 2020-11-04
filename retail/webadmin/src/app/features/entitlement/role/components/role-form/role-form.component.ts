e579ed578ac3fee83142da8080dc85e6af7297ec
AzureAD\RashidAslam <rashidaslam75@gmail.com>
2020-11-05 00:26:03 +0500

    fix: removed debugger

--- retail/webadmin/src/app/features/entitlement/role/components/role-form/role-form.component.ts
+++ retail/webadmin/src/app/features/entitlement/role/components/role-form/role-form.component.ts
@@ -39,7 +39,6 @@ export class RoleFormComponent extends BaseComponent implements OnDestroy, OnIni
     }
 
     ngOnInit(): void {
-        debugger
         const totalPermissions = this.data.permissions.map((x) => x.record_type);
         this.displayedColumns = this.displayedColumns.concat(totalPermissions);
      
