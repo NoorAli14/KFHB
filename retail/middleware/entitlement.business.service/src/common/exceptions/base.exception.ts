ea9267975e88c4ddcf89fbec7a412919369ccaa1
Faizan AH <faizan@aiondigital.com>
2020-11-08 01:09:06 +0500

    feat: [APS-738] enbale validation exception handler

---
+++ retail/middleware/entitlement.business.service/src/common/exceptions/base.exception.ts
@@ -0,0 +1,13 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+interface IError {
+    name: string;
+    message: string;
+    errorCode?: string;
+    developerMessage?: string;
+}
+export class BaseException extends HttpException {
+    constructor(status: number, error?: IError) {
+        super((Array.isArray(error) ? error : [error]), status);
+        this.name = error.name;
+    }
+}
