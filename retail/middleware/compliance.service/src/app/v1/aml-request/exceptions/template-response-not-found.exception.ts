6ae9b741e801c05d414b22ee6e3869fec5e42bdb
Faizan AH <faizan@aiondigital.com>
2020-11-09 02:02:16 +0500

    docs: [APS-739] typo fix

--- retail/middleware/compliance.service/src/app/v1/aml-request/exceptions/template-response-not-found.exception.ts
+++ retail/middleware/compliance.service/src/app/v1/aml-request/exceptions/template-response-not-found.exception.ts
@@ -4,7 +4,6 @@ export class TemplateResponseNotFoundException extends BaseException {
   constructor(user_id?: string) {
     super(HttpStatus.NOT_FOUND, {
       message: 'KYC detail missing',
-      errorCode: 'CMP-1032',
       name: 'TEMPLATE_RESPONSE_NOT_FOUND',
       developerMessage: `Template response not found against user with Id [${user_id}]`,
     });
