b93c2a2180aecf00542c0dd482c91bd898b3d272
Muhammad Saeed Raza <sraza@aiondigital.com>
2020-11-15 11:29:58 +0500

    docs: [APS-744] husky magic

--- retail/middleware/usermanagement.service/src/core/database/migrations/20200719231448_create_module_table.ts
+++ retail/middleware/usermanagement.service/src/core/database/migrations/20200719231448_create_module_table.ts
@@ -1,13 +1,22 @@
 import * as Knex from 'knex';
 import { TABLE, DATABASE_UUID_METHOD, STATUS } from '@common/index';
 export function up(knex: Knex): any {
-    return knex.schema.createTable(TABLE.MODULE, table => {
-        table.uuid('id').primary().defaultTo(DATABASE_UUID_METHOD(knex));
-        table.string('name').notNullable();
-        table.uuid('parent_id');
-        table.string('status').defaultTo(STATUS.ACTIVE).notNullable();
-        table.timestamp('created_on', { useTz: true }).defaultTo(knex.fn.now()).notNullable();
-        table.string('created_by').notNullable();
+  return knex.schema.createTable(TABLE.MODULE, table => {
+    table
+      .uuid('id')
+      .primary()
+      .defaultTo(DATABASE_UUID_METHOD(knex));
+    table.string('name').notNullable();
+    table.uuid('parent_id');
+    table
+      .string('status')
+      .defaultTo(STATUS.ACTIVE)
+      .notNullable();
+    table
+      .timestamp('created_on', { useTz: true })
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table.string('created_by').notNullable();
 
     //index
     table.index(['name'], `${TABLE.MODULE}_NAME_INDEX`);
--- retail/middleware/usermanagement.service/src/core/database/migrations/20200824175601_create_leave_type_table.ts
+++ retail/middleware/usermanagement.service/src/core/database/migrations/20200824175601_create_leave_type_table.ts
@@ -2,11 +2,20 @@ import * as Knex from 'knex';
 import { TABLE, DATABASE_UUID_METHOD, STATUS } from '@common/index';
 export function up(knex: Knex): any {
   return knex.schema.createTable(TABLE.LEAVE_TYPE, table => {
-    table.uuid('id').primary().defaultTo(DATABASE_UUID_METHOD(knex));
+    table
+      .uuid('id')
+      .primary()
+      .defaultTo(DATABASE_UUID_METHOD(knex));
     table.uuid('tenant_id').notNullable();
     table.string('name').notNullable();
-    table.string('status').defaultTo(STATUS.ACTIVE).notNullable();
-    table.timestamp('created_on', { useTz: true }).defaultTo(knex.fn.now()).notNullable();
+    table
+      .string('status')
+      .defaultTo(STATUS.ACTIVE)
+      .notNullable();
+    table
+      .timestamp('created_on', { useTz: true })
+      .defaultTo(knex.fn.now())
+      .notNullable();
     table.string('created_by').notNullable();
     table.timestamp('updated_on', { useTz: true }).defaultTo(knex.fn.now());
     table.string('updated_by');
