49b2914d5e8b7902786722ad3862a44a16cd749a
Muhammad Saeed Raza <sraza@aiondigital.com>
2020-11-16 11:37:27 +0500

    feat: [APS-744] added system-audit-log events from login

--- retail/middleware/usermanagement.service/src/app/v1/login/login.module.ts
+++ retail/middleware/usermanagement.service/src/app/v1/login/login.module.ts
@@ -5,9 +5,16 @@ import { LoginResolver } from '@app/v1/login/login.resolver';
 import { LoginService } from '@app/v1/login/login.service';
 import { RepositoryModule } from '@core/repository/repository.module';
 import { UserRepository } from '@core/repository';
+import { SystemAuditLogService } from '@app/v1/system-audit-log/system-audit-log.service';
 
 @Module({
   imports: [RepositoryModule],
-  providers: [LoginResolver, LoginService, Encrypter, UserRepository],
+  providers: [
+    LoginResolver,
+    LoginService,
+    Encrypter,
+    UserRepository,
+    SystemAuditLogService,
+  ],
 })
 export class LoginModule {}
--- retail/middleware/usermanagement.service/src/app/v1/login/login.service.ts
+++ retail/middleware/usermanagement.service/src/app/v1/login/login.service.ts
@@ -5,10 +5,16 @@ import { ITenant } from '@common/interfaces';
 import { UserRepository } from '@core/repository';
 import { User } from '../users/user.model';
 import { InvalidEmailPasswordException } from './exceptions/invalid-email-password.exception';
+import { SystemAuditLogService } from '@app/v1/system-audit-log/system-audit-log.service';
+import { SYSTEM_AUDIT_CODES, SYSTEM_AUDIT_LOG_STRINGS } from '@rubix/common';
 
 @Injectable()
 export class LoginService {
-  constructor(private userDB: UserRepository, private encrypter: Encrypter) {}
+  constructor(
+    private userDB: UserRepository,
+    private encrypter: Encrypter,
+    private systemAuditLogService: SystemAuditLogService,
+  ) {}
 
   async verifyUser(
     tenant: ITenant,
@@ -26,8 +32,19 @@ export class LoginService {
       this.encrypter.comparePassword(input.password, user.password_digest)
     ) {
       delete user.password_digest;
+      await this.systemAuditLogService.create(tenant.id, {
+        audit_code: SYSTEM_AUDIT_CODES.USER_LOGIN,
+        audit_text:
+          SYSTEM_AUDIT_LOG_STRINGS.LOGIN_SUCCESS + ` with email ${input.email}`,
+        user_id: user.id,
+      });
       return user;
     }
+    await this.systemAuditLogService.create(tenant.id, {
+      audit_code: SYSTEM_AUDIT_CODES.USER_LOGIN,
+      audit_text:
+        SYSTEM_AUDIT_LOG_STRINGS.LOGIN_FAILED + ` with email ${input.email}`,
+    });
     throw new InvalidEmailPasswordException(input.email);
   }
 }
