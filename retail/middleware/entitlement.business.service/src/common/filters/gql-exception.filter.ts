f1cd4b97bd4040420dd02e7e22dd70cf5575a0be
Muhammad Saeed Raza <sraza@aiondigital.com>
2020-11-18 23:18:07 +0500

    feat: [APS-833] added functions in all services to return proper sentences in exceptions instead of snake case words

--- retail/middleware/entitlement.business.service/src/common/filters/gql-exception.filter.ts
+++ retail/middleware/entitlement.business.service/src/common/filters/gql-exception.filter.ts
@@ -1,4 +1,4 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { GqlException } from '@common/exceptions';
 
--- retail/middleware/entitlement.business.service/src/common/filters/http-exception.filter.ts
+++ retail/middleware/entitlement.business.service/src/common/filters/http-exception.filter.ts
@@ -9,6 +9,17 @@ import { Request, Response } from 'express';
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException | any, host: ArgumentsHost): void {
+    if (Array.isArray(exception['response'])) {
+      exception['response'].map(validationError => {
+        const property: string = validationError['property'];
+        if (property?.includes('_')) {
+          const constraints = validationError['constraints'];
+          Object.keys(constraints).map(function (constraintKey) {
+            constraints[constraintKey] = constraints[constraintKey].replace('_', ' ');
+          });
+        }
+      });
+    }
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
