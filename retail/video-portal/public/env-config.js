5d9dc3cf47e30690769c4a911c2a465fb95780c3
FatemaAhmedZainaldin <fatema@aiondigital.com>
2020-12-08 16:10:23 +0300

    create account url changed

--- retail/video-portal/public/env-config.js
+++ retail/video-portal/public/env-config.js
@@ -9,7 +9,7 @@
     window["_env_"]["RUBIX_BASE_URL"] = "https://rubix-dev01.conduit-aiondigital.com/api/v1"
     window["_env_"]["REACT_APP_XMPP_PORT"] = "5280"
     window["_env_"]["REACT_APP_SSL"] = "true"
-    window["_env_"]["ACCOUNT_BASE_URL"] = "https://aionj2middlewaredev.uksouth.cloudapp.azure.com"
+    window["_env_"]["ACCOUNT_BASE_URL"] = "http://10.5.1.37"
  
  
   })(this);
