23a7256804048b3962cb78adf8cce2354b2039cf
Fatema Zainaldin <63390564+FatemaAhmedZainaldin@users.noreply.github.com>
2021-04-30 22:06:29 +0300

    update evn variable

--- retail/webadmin/src/environments/environment.prod.ts
+++ retail/webadmin/src/environments/environment.prod.ts
@@ -11,7 +11,7 @@ export const environment = {
     VIDEO_URL: window["env"]["ENV_RBX_VIDEO_URL"],
     RETAIL_API_BASE_URL: window["env"]["ENV_RBX_RETAIL_API_BASE_URL"],
     CORPORATE_API_BASE_URL: window["env"]["ENV_RBX_CORPORATE_API_BASE_URL"],
-    API_BASE_URL: 'http://172.18.4.21/',
+    API_BASE_URL: 'http://172.18.4.21',
   };
   
   setTimeout(() => {
