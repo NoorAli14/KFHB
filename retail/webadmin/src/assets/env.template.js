eeac672260e070f7b07c4692961d94bbfac6b012
Fatema Zainaldin <63390564+FatemaAhmedZainaldin@users.noreply.github.com>
2021-05-02 12:55:12 +0300

    changed env variable

--- retail/webadmin/src/assets/env.template.js
+++ retail/webadmin/src/assets/env.template.js
@@ -7,7 +7,7 @@
   window["env"]["ENV_RBX_TENANT_ID"] = "${ENV_RBX_TENANT_ID}";
   window["env"]["ENV_RBX_VIDEO_URL"] = "${ENV_RBX_VIDEO_URL}";
   window["env"]["ENV_RBX_RETAIL_API_BASE_URL"] = "${ENV_RBX_RETAIL_API_BASE_URL}";
-  window["env"]["ENV_RBX_API_BASE_URL"] = "${ENV_RBX_API_BASE_URL}";
   window["env"]["ENV_RBX_CORPORATE_API_BASE_URL"] = "${ENV_RBX_CORPORATE_API_BASE_URL}";
+  window["env"]["J2_BASE_URL"] = "${J2_BASE_URL}";
   
 })(this);
